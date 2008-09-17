29d2332763e8d05a4021ee3d7da444282faaf3e6
lvh <lvh@51cac136-f83d-432d-bb6f-125e83a60a2d>
2008-09-17 09:29:53 +0000

    createed and added the file for the PTIPool
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@2367 51cac136-f83d-432d-bb6f-125e83a60a2d

---
+++ src/dbc/opensearch/components/pti/PTIPool.java
@@ -0,0 +1,11 @@
+package dbc.opensearch.components.pti;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.*;
+import org.apache.log4j.xml.DOMConfigurator;
+import org.apache.log4j.Logger;
+
+public class PTIPool {
+
+}
