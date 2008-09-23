e0bd661a56326ad8cf48a9b0f9c162579bf0bfa1
shm <shm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2008-09-23 14:41:46 +0000

    removde some import lines and made some comments
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@2442 51cac136-f83d-432d-bb6f-125e83a60a2d

--- src/dbc/opensearch/components/pti/PTIPool.java
+++ src/dbc/opensearch/components/pti/PTIPool.java
@@ -1,9 +1,5 @@
 package dbc.opensearch.components.pti;
 
-import com.mallardsoft.tuple.Tuple;
-import com.mallardsoft.tuple.Pair;
-
-import dbc.opensearch.tools.Processqueue;
 import dbc.opensearch.tools.FedoraHandler;
 
 import org.compass.core.Compass;
@@ -25,58 +21,65 @@ import org.apache.log4j.xml.DOMConfigurator;
 import org.apache.log4j.Logger;
 import org.apache.commons.configuration.ConfigurationException;
 
-
 import java.net.MalformedURLException;
 import java.net.UnknownHostException;
 import javax.xml.rpc.ServiceException;
 
+
+/**
+ * \brief PTIPool is a threadpool managing PTI threads. It has a
+ * createAndJoin method for stating threads, and a private method
+ * providing a compassSession
+ */
 public class PTIPool {
 
     private ExecutorService threadExecutor; /**The threadpool */
     private static volatile Compass theCompass;
     private static volatile FedoraHandler theFedoraHandler;
+   
+    Logger log = Logger.getLogger("PTIPool");
+
     /**
-     * log
+     * \brief Constructor initializes the threadpool 
+     * @param numberOfThreads The number of threads in the pool
      */
-    private static final Logger log = Logger.getRootLogger();
-
     public PTIPool( int numberOfThreads ) throws ConfigurationException, MalformedURLException, UnknownHostException, ServiceException, IOException {
-
-        log.debug( String.format( "Number of threads = %s", numberOfThreads ) );
+        log.debug( String.format( "Entering PTIPool(NumberOfThreads=%s)", numberOfThreads ) );
+        
         // Securing nuberOfThreads > 0
         if ( numberOfThreads <= 0 ){
             /** \todo Find suitable exception */
-            log.fatal( String.format( "Number of threads specified was 0, quite absurd." ) );
+            log.fatal( String.format( "Number of threads specified was 0 or less." ) );
             throw new ConfigurationException( "Refusing to construct empty PTIPool" );
         }
         
         log.debug( String.format( "Starting the threadPool" ) );
         threadExecutor = Executors.newFixedThreadPool( numberOfThreads );
 
-        log.debug( String.format( "Setting up the Compass object" ) );
+        log.debug( "Setting up the Compass object" );
         if( theCompass == null ){
+            log.debug( "No session found. Setting up a new one. getting parameters." );
+            
             CompassConfiguration conf = new CompassConfiguration();
-
-
-            /** \todo: FIXME hardcoded values. Should come from startup config file */
             URL cfg = getClass().getResource("/compass.cfg.xml");
             URL cpm = getClass().getResource("/xml.cpm.xml");
             log.debug( String.format( "Compass configuration=%s", cfg.getFile() ) );
             log.debug( String.format( "XSEM mappings file   =%s", cpm.getFile() ) );
-
             File cpmFile = new File( cpm.getFile() );
 
             conf.configure( cfg );
             conf.addFile( cpmFile );
-
             theCompass = conf.buildCompass();
+            
+            log.debug( "Compass build" );
         }
 
-        log.debug( String.format( "Setting up the FedoraHandler" ) );
+        log.debug( "Setting up the FedoraHandler" );
+        
         if( theFedoraHandler == null) {
+            log.debug( "No fedorahandler found. Setting up a new one." );
             theFedoraHandler = new FedoraHandler();
         }
-
         log.info( "The PTIPool has been constructed" );
     }
 
@@ -87,39 +90,32 @@ public class PTIPool {
      * that the PTIPoolAdm uses for keeping track of which digitalobjects
      * are in process
      */
-
-    /** \todo find better Exception to throw*/
-
     public FutureTask createAndJoinThread (String fHandle, String itemID )throws ConfigurationException, ClassNotFoundException{
-
+        log.debug( String.format( "entering createAndJoinThreads( fhandle=%s, itemID=%s )", numberOfThreads, itemID ) );
+        
         CompassSession session = null;
         FutureTask future = null;
-        // try{
-            log.debug( String.format( "Getting CompassSession" ) );
-            session = getSession();
-            log.debug( String.format( "Constructing FutureTask on PTI" ) );
-            future = new FutureTask( new PTI( session, fHandle, itemID, theFedoraHandler ));
-        // }catch(RuntimeException re){
-        //     log.fatal( String.format( "RunTimeException occured in createAndJoinThread" ) );
-        //     throw new RuntimeException( re.getMessage() );
-        // }catch(ConfigurationException ce){
-        //     log.fatal( String.format( "ConfigurationException occured in createAndJoinThread" ) );
-        //     throw new ConfigurationException( ce.getMessage() );
-        // }
-
+        
+        log.debug( "Getting CompassSession" );
+        session = getSession();
+        
+        log.debug( "Constructing FutureTask on PTI" );
+        future = new FutureTask( new PTI( session, fHandle, itemID, theFedoraHandler ));
+        
         log.debug( String.format( "Submitting the FutureTask to the threads" ) );
         threadExecutor.submit(future);
-        log.debug( String.format( "Returning the FutureTask" ) );
+        
+        log.debug( "FutureTask submitted" );
         return future;
     }
-
 
     /**
      * Returns a Compass session, and checks whether its instantiated.
      */
     public CompassSession getSession() throws RuntimeException {
+        log.debug( "Entering getSession" );
         if( theCompass == null) {
-            log.fatal( String.format( "getSession was called on an object that in the meantime went null. Aborting" ) );
+            log.fatal( "getSession was called on an object that in the meantime went null. Aborting" );
             throw new RuntimeException( "getSession was called on an object that in the meantime went null. Aborting" );
         }
         CompassSession s = theCompass.openSession();
