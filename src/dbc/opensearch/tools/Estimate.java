dd8cea4e2789c5ac7fa2eaab6f9b80e62f4dbe66
shm <shm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2008-09-23 12:28:56 +0000

    updated Estimate with comments and such
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@2436 51cac136-f83d-432d-bb6f-125e83a60a2d

--- src/dbc/opensearch/tools/Estimate.java
+++ src/dbc/opensearch/tools/Estimate.java
@@ -23,8 +23,8 @@ public class Estimate extends DBConnection {
 
     /**
      * Log
-     */
-    private static final Logger log = Logger.getRootLogger();
+     */    
+    Logger log = Logger.getLogger("Estimate");
 
     /**
      * Constructor
@@ -34,71 +34,54 @@ public class Estimate extends DBConnection {
     }
 
     /**
-     * getestimate retrieves estimate from statisticDB table.
+     * \brief getEstimate retrieves estimate from statisticDB table.
+     * @param mimeType The mimeType of the element were trying to estimate processtime for
+     * @param length length in bytes of the element were trying to estimate processtime for
      * @return the processtime estimate.
-     * If the return value == 0l, no estimate is made, caller must
-     * check this, but then an exception should have been thrown
      */
 
     /** \todo: construct proper exception like an connnectionerrorexception-type thing */
     public float getEstimate( String mimeType, long length ) throws SQLException, NoSuchElementException, ClassNotFoundException {
+        log.debug( String.format( "estimate.getEstimate(mimeType=%s, length=%s) called", mimeType, length ) );
 
-        log.info( String.format( "in estimate(). Length=%s",length ) );
+        con = establishConnection();
 
         float average_time = 0f;
         ResultSet rs = null;
-
-        // 20: open database connection
-        log.debug( "Establishing connection to statisticDB" );
-
-        Connection con = null;
-       
-        con = establishConnection();
-       
-        // 25: create statement
         Statement stmt = null;
-
         String sqlQuery = String.format( "SELECT processtime, dataamount FROM statisticDB WHERE mimetype = '%s'", mimeType );
-
-        log.info( String.format( "SQL Query == %s", sqlQuery ) );
+        log.debug( String.format( "query database with %s ", sqlQuery ) );
 
         try{
             stmt = con.createStatement( ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE );
-        }
-        catch(SQLException sqe) {
-            log.fatal( "SQLException: " + sqe.getMessage() );
-            throw new SQLException( sqe.getMessage() );
-        }
-
-        try{
             rs = stmt.executeQuery ( sqlQuery );
-            log.debug( String.format( "statisticDB queried with \"%s\"", sqlQuery ) );
-        }
-        catch(SQLException sqe) {
-            log.fatal( "SQLException: " + sqe.getMessage() );
-            throw new SQLException( sqe.getMessage() );
-        }
-        if( rs == null ){
-            throw new NoSuchElementException( String.format( "We didnt get anything from the database, the mimetype \"%s\"is unknown.", mimeType ) );
-        }
-        rs.last();
-        int rowCount = rs.getRow();
-        log.debug( String.format( "rows = %s", rowCount ) );
+            
+            if( rs == null ){
+                throw new NoSuchElementException( String.format( "We didnt get anything from the database, the mimetype \"%s\"is unknown.", mimeType ) );
 
-        if( rowCount != 1 ){
-            throw new SQLException( String.format( "Count if rows is different from 1. RowCount==%s", rowCount) );
+            }
+            
+            rs.last();
+            int rowCount = rs.getRow();
+            log.debug( String.format( "rows = %s", rowCount ) );
+            
+            if( rowCount != 1 ){
+                throw new SQLException( String.format( "Count if rows is different from 1. RowCount==%s", rowCount) );
+            }
+            rs.first();            
+            long p = rs.getLong( "processtime" );
+            long d = rs.getLong( "dataamount" );
+            if ( d != 0l && p != 0l ){ // if either is zero
+                average_time = ( ( (float)p / d ) * length );
+            }
+            
+            log.debug( String.format( "processtime=%s dataamount=%s, averagetime=%s", p, d, average_time ) );
         }
-        rs.first();
-
-        long p = rs.getLong( "processtime" );
-        long d = rs.getLong( "dataamount" );
-        
-        log.info( String.format( "processtime=%s dataamount=%s average time for mimetype %s = %s", p, d, mimeType, average_time ) );
-
-        if ( d != 0l && p != 0l ){
-            average_time = ( ( (float)p / d ) * length );
-            log.debug( String.format( "\nprocesstime=%s\ndataamount=%s\np/d=%s\naverage time for mimetype %s = %s", p, d, p/d, mimeType, average_time ) );
+        finally{
+            stmt.close();
+            con.close();
         }
+        log.info( String.format( "Obtained average processing time=%s",average_time) );
         return average_time;
     }
 
@@ -109,30 +92,24 @@ public class Estimate extends DBConnection {
      * @param time is time in millisecs that it took to proces the object
      */
     public void updateEstimate( String mimeType, long length, long time ) throws SQLException, ClassNotFoundException{
-        log.debug( String.format( "Entering UpdateEstimate(mimeType = %s, length = %s, time = %s)", mimeType, length, time ) );
+        log.debug( String.format( "UpdateEstimate(mimeType = %s, length = %s, time = %s) called", mimeType, length, time ) );
 
-        log.debug( "Entering UpdateEstimate" );
-        
-        // open database connection
-        log.debug( "Establishing connection to statisticDB" );
-
-        Connection con = null;
-
-            con = establishConnection();
-        // create query
-        String sqlQuery = String.format( "UPDATE statisticDB SET processtime = processtime+%s, dataamount = dataamount+%s WHERE mimetype = '%s'", time, length, mimeType);
-        log.debug( String.format( "SQL Query == %s", sqlQuery ) );
-        
-        // Write new estimate values to database
+        con = establishConnection();
+ 
         Statement stmt = null;
+        String sqlQuery = String.format( "UPDATE statisticDB "+
+                                         "SET processtime = processtime+%s, dataamount = dataamount+%s "+
+                                         "WHERE mimetype = '%s'", time, length, mimeType);
+        log.debug( String.format( "query database with %s ", sqlQuery ) );
+        
         try{      
             stmt = con.createStatement();
             stmt.executeUpdate( sqlQuery );
-            log.debug( "New estimate written to database" );
         }
-        catch(SQLException sqe) {
-            log.fatal( "SQLException: " + sqe.getMessage() );
-            throw new SQLException( sqe );
-        }        
-     }
+        finally{
+            stmt.close();
+            con.close();
+        }
+        log.info( "estimate Updated" );
+    }
 }
