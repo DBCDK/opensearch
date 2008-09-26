60fe51d32f02ecf2f790d529b194b23d8eceeec9
stm <stm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2008-09-26 09:44:02 +0000

    test of programmatic DigitalObjects and Datastreams
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@2470 51cac136-f83d-432d-bb6f-125e83a60a2d

--- src/dbc/opensearch/components/pti/tests/fedora_test/DigitalObjectTest.java
+++ src/dbc/opensearch/components/pti/tests/fedora_test/DigitalObjectTest.java
@@ -1,10 +1,12 @@
 package dbc.opensearch.components.pti.tests.fedora_test;
 
 import fedora.server.storage.types.BasicDigitalObject;
-//import fedora.server.storage.types.Datastream;
+import fedora.server.storage.types.DigitalObject;
+
+import fedora.server.storage.types.Datastream;
 import fedora.server.storage.types.DatastreamDef;
 import fedora.server.types.gen.DatastreamControlGroup;
-import fedora.server.types.gen.Datastream;
+//import fedora.server.types.gen.Datastream;
 
 import fedora.server.storage.translation.FOXML1_1DOSerializer;
 
@@ -15,70 +17,135 @@ import fedora.common.Constants;
 // for addDatastreamToDO:
 import fedora.client.Administrator;
 
-
+import java.util.Date;
 
 /**
  *
  */
 public class DigitalObjectTest {
+
+    BasicDigitalObject bdo;
+    fedora.server.access.FedoraAPIA apia;
+    fedora.server.management.FedoraAPIM apim;
     /**
      *
      */
-    public DigitalObjectTest() {
-        BasicDigitalObject bdo = new BasicDigitalObject();
+    public DigitalObjectTest()throws java.net.MalformedURLException, javax.xml.rpc.ServiceException, java.io.IOException{
+        bdo = new BasicDigitalObject();
         Datastream ds = new Datastream();
+
+        fedora.client.FedoraClient fc = new fedora.client.FedoraClient( "","","");
+        apia = fc.getAPIA();
+        apim = fc.getAPIM();
     }
 
-    private static Datastream[] MakeBlankRelsExtDatastream() {
-        Datastream[] ds = new Datastream[1];
-        ds[0] =
-            new Datastream(DatastreamControlGroup.fromValue("X"),
-                           "RELS-EXT",
-                           "RELS-EXT.0",
-                           null,
-                           "RDF Statements about this object",
-                           true,
-                           "application/rdf+xml",
-                           Constants.RELS_EXT1_0.uri,
-                           null,
-                           0,
-                           "A",
-                           null,
-                           null,
-                           null);
+    private DigitalObject constructDO(){
+
+        bdo.setPid( "dbc:1" );
+        bdo.setState( "Active" );
+        bdo.setLabel( "dbc faktalink" );
+        bdo.setOwnerId( "fedoraAdmin" );
+        bdo.setCreateDate( new Date() );
+        bdo.setLastModDate( new Date() );
+        bdo.setNew( true );
+
+        bdo.addDatastreamVersion( DigitalObjectTest.MakeBlankRelsExtDatastream(), true );
+
+        String[] altId = new String[]{""};
+
+        bdo.addDatastreamVersion( DigitalObjectTest.constructDatastream( DatastreamControlGroup.M, "", "", altId, "", false, "", "", new Date(), 1l, "", "", "", ""), true );
+
+        return bdo;
+    }
+
+    private static Datastream constructDatastream( DatastreamControlGroup controlGroup,
+                                                   String ID,
+                                                   String versionID,
+                                                   String[] altIDs,
+                                                   String label,
+                                                   boolean versionable,
+                                                   String MIMEType,
+                                                   String formatURI,
+                                                   Date createDate,
+                                                   long size,
+                                                   String state,
+                                                   String location,
+                                                   String checksumType,
+                                                   String checksum ){
+
+        Datastream ds = new Datastream();
+
+        ds.DSControlGrp   = DatastreamControlGroup.M.toString();
+        ds.DatastreamID     = ID;
+        ds.DSVersionID      = versionID;
+        ds.DatastreamAltIDs = altIDs;
+        ds.DSLabel          = label;
+        ds.DSVersionable    = versionable;
+        ds.DSMIME           = MIMEType;
+        ds.DSFormatURI      = formatURI;
+        ds.DSCreateDT       = createDate;
+        ds.DSSize           = size;
+        ds.DSState          = state;
+        ds.DSLocation       = location;
+        ds.DSChecksumType   = checksumType;
+        ds.DSChecksum       = checksum;
+
+        return ds;
+
+    }
+
+
+    private static Datastream MakeBlankRelsExtDatastream() {
+
+        Datastream ds = new Datastream();
+
+        ds.DSControlGrp   = DatastreamControlGroup.X.toString();
+        ds.DatastreamID     = "RELS-EXT";
+        ds.DSVersionID      = "RELS-EXT.0";
+        ds.DatastreamAltIDs = null;
+        ds.DSLabel          = "RDF Statements about this object";
+        ds.DSVersionable    = true;
+        ds.DSMIME           = "application/rdf+xml";
+        ds.DSFormatURI      = Constants.RELS_EXT1_0.uri;
+        ds.DSCreateDT       = null;
+        ds.DSSize           = 0;
+        ds.DSState          = "A";
+        ds.DSLocation       = null;
+        ds.DSChecksumType   = null;
+        ds.DSChecksum       = null;
+
         return ds;
     }
 
-    private static String addDatastreamToDO(
-                                            String pid,
-                                            String dsid,
-                                            String[] altIDs,
-                                            String label,
-                                            boolean versionable,
-                                            String mimeType,
-                                            String formatURI,
-                                            String location,
-                                            String state,
-                                            String checksumType,
-                                            String logMessage) throws java.rmi.RemoteException{
+    private String addDatastreamToExistingPid(
+                                              String pid,
+                                              String dsid,
+                                              String[] altIDs,
+                                              String label,
+                                              boolean versionable,
+                                              String mimeType,
+                                              String formatURI,
+                                              String location,
+                                              String state,
+                                              String checksumType,
+                                              String logMessage) throws java.rmi.RemoteException{
 
-        return Administrator.APIM.addDatastream(pid,
-                                                dsid,
-                                                altIDs,
-                                                label,
-                                                versionable, // DEFAULT_VERSIONABLE
-                                                mimeType,
-                                                formatURI,
-                                                location,
-                                                "X",
-                                                state,
-                                                checksumType,
-                                                null, // checksum type and checksum
-                                                logMessage); // DEFAULT_LOGMESSAGE
-
+        return apim.addDatastream(pid,
+                                  dsid,
+                                  altIDs,
+                                  label,
+                                  versionable, // DEFAULT_VERSIONABLE
+                                  mimeType,
+                                  formatURI,
+                                  location,
+                                  "X",
+                                  state,
+                                  checksumType,
+                                  null, // checksum type and checksum
+                                  logMessage); // DEFAULT_LOGMESSAGE
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args)throws java.net.MalformedURLException, javax.xml.rpc.ServiceException, java.io.IOException{
         DigitalObjectTest dot = new DigitalObjectTest();
     }
 
