9f46b40e12d4fd9e79d63dd733f634be15bd3b27
shm <shm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2008-10-06 10:38:25 +0000

    some test, not working properly yet
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@2569 51cac136-f83d-432d-bb6f-125e83a60a2d

--- src/dbc/opensearch/tools/tests/ProcessqueueTest.java
+++ src/dbc/opensearch/tools/tests/ProcessqueueTest.java
@@ -7,75 +7,81 @@ import com.mockrunner.jdbc.StatementResultSetHandler;
 import com.mockrunner.mock.jdbc.MockResultSet;
 import com.mockrunner.mock.jdbc.MockStatement;
 
+import com.mockrunner.jdbc.CallableStatementResultSetHandler;
+
+
+
+
+
 import org.apache.commons.configuration.ConfigurationException;
 import java.lang.ClassNotFoundException;
 import java.sql.SQLException;
 import java.util.NoSuchElementException;
 
-
 import com.mallardsoft.tuple.Tuple;
 import com.mallardsoft.tuple.Triple;
 
-
-
 public class ProcessqueueTest extends BasicJDBCTestCaseAdapter {
-    
-   
-    private void TestEstablishConneciton(){
 
+    MockConnection connection;
+    StatementResultSetHandler statementHandler;
+    MockResultSet result;
+    
+    Processqueue processqueue;
+
+    private CallableStatementResultSetHandler callableStatementHandler;
+
+    protected void setUp() throws Exception {
+
+        super.setUp();
+        connection = getJDBCMockObjectFactory().getMockConnection();
+   
+        statementHandler = connection.getStatementResultSetHandler();
+        result = statementHandler.createResultSet();
+        processqueue = new Processqueue();
     }
 
+    //    private void TestEstablishConneciton(){}
 
     public  void testPopFromEmptyProcessqueue() throws ConfigurationException, ClassNotFoundException, SQLException {
-        
-        MockConnection connection = getJDBCMockObjectFactory().getMockConnection();
-        StatementResultSetHandler statementHandler = connection.getStatementResultSetHandler();
 
-        //Processqueue processqueue = new Processqueue();
-        MockResultSet result = statementHandler.createResultSet();
         statementHandler.prepareGlobalResultSet( result );
-        Processqueue processqueue = new Processqueue();
-        
+
         try{
             Triple<String, Integer, String> triple = processqueue.pop();
             fail("Should have gotten NoSuchElementException - returned zero rows removed from processqueue table");
         }
         catch(NoSuchElementException nse){
             // Expected - intentional
-        }        
+        }
         verifyNotCommitted();
         verifyAllStatementsClosed();
-        verifyConnectionClosed();    
+        verifyConnectionClosed();
     }
 
-    //////////////////
-    public void testPopFromProcessqueueWithActiveElements() throws ConfigurationException, ClassNotFoundException, SQLException {
-    
-//         MockConnection connection = getJDBCMockObjectFactory().getMockConnection();
-//         StatementResultSetHandler statementHandler = connection.getStatementResultSetHandler();       
-//         MockResultSet result = statementHandler.createResultSet();
 
-//         result.addColumn("queueid", new Integer[] { 1 });
-//         result.addColumn("fedoraHandle", new String[] {"testfedoraHandle1" });
-//         result.addColumn("itemID", new String[] {"testItem1" });
-//         result.addColumn("processing", new String[] { "N" });
-//         statementHandler.prepareGlobalResultSet( result );
+
+    /////////////////////////
+    public void testPopFromProcessqueueWithActiveElements() throws ConfigurationException, ClassNotFoundException, SQLException {
+
+        callableStatementHandler = getJDBCMockObjectFactory().getMockConnection().getCallableStatementResultSetHandler();
+        MockResultSet procResult = callableStatementHandler.createResultSet();
+        procResult.addColumn("fedoraHandle", new String[] {"testfedoraHandle1"  });
+        procResult.addColumn("queueid", new Integer[] { 1  });
+        procResult.addColumn("processing", new String[] { "N"  });
+        procResult.addColumn("itemID", new String[] {"testItem1"  });
+        callableStatementHandler.prepareResultSet( "call proc_prod", procResult );
         
-//         Processqueue processqueue = new Processqueue();
-//         Triple<String, Integer, String> triple = processqueue.pop();
-//         System.out.println( triple.toString() );
-    
+        Triple<String, Integer, String> triple = processqueue.pop();
+
+        System.out.println( triple.toString() );
+
     }
 
     private void TestPopFromProcessqueueWithNoActiveElements(){}
     //////////////////
 
     public void testPushToProcessqueue() throws ConfigurationException, ClassNotFoundException, SQLException {
-        
-        MockConnection connection = getJDBCMockObjectFactory().getMockConnection();
-        StatementResultSetHandler statementHandler = connection.getStatementResultSetHandler();
-        MockStatement mockStatement = new MockStatement( connection );
-        Processqueue processqueue = new Processqueue();
 
         String testFedoraHandle = "testFedoraHandle";
         String testItemID = "testItemID";
@@ -83,38 +89,26 @@ public class ProcessqueueTest extends BasicJDBCTestCaseAdapter {
 
         String sql_query = (  String.format( "INSERT INTO processqueue(queueid, fedorahandle, itemID, processing) "+
                                              "VALUES(processqueue_seq.nextval ,'%s','%s','N')", testFedoraHandle, testItemID ) );
-        verifySQLStatementExecuted( sql_query );       
+        verifySQLStatementExecuted( sql_query );
         verifyCommitted();
         verifyAllStatementsClosed();
         verifyConnectionClosed();
     }
-    
-    public void testCommitWithValidQueueID() throws ConfigurationException, ClassNotFoundException, SQLException{
-        
-        MockConnection connection = getJDBCMockObjectFactory().getMockConnection();
-        StatementResultSetHandler statementHandler = connection.getStatementResultSetHandler();
-        MockStatement mockStatement = new MockStatement( connection );
-        Processqueue processqueue = new Processqueue();
 
-        statementHandler.prepareGlobalUpdateCount( 1 ); 
+    public void testCommitWithValidQueueID() throws ConfigurationException, ClassNotFoundException, SQLException{
+        statementHandler.prepareGlobalUpdateCount( 1 );
         int testQueueID = 10;
 
         processqueue.commit( testQueueID );
         String sql_query = String.format( "DELETE FROM processqueue WHERE queueid = %s", testQueueID );
-        verifySQLStatementExecuted( sql_query );       
+        verifySQLStatementExecuted( sql_query );
         verifyCommitted();
         verifyAllStatementsClosed();
         verifyConnectionClosed();
     }
 
     public void testCommitWithInvalidQueueID() throws ConfigurationException, ClassNotFoundException, SQLException{
-        
-        MockConnection connection = getJDBCMockObjectFactory().getMockConnection();
-        StatementResultSetHandler statementHandler = connection.getStatementResultSetHandler();
-        MockStatement mockStatement = new MockStatement( connection );
-        Processqueue processqueue = new Processqueue();
-
-        statementHandler.prepareGlobalUpdateCount( 0 ); 
+        statementHandler.prepareGlobalUpdateCount( 0 );
         int testQueueID = 10;
 
         try{
@@ -124,41 +118,31 @@ public class ProcessqueueTest extends BasicJDBCTestCaseAdapter {
         catch(NoSuchElementException nse){
             // Expected - intentional
         }
-        
         String sql_query = String.format( "DELETE FROM processqueue WHERE queueid = %s", testQueueID );
-        verifySQLStatementExecuted( sql_query );       
-        
+        verifySQLStatementExecuted( sql_query );
+
         verifyNotCommitted();
         verifyAllStatementsClosed();
-        verifyConnectionClosed();    
+        verifyConnectionClosed();
     }
     public void testRollbackWithValidQueueID() throws ConfigurationException, ClassNotFoundException, SQLException{
-                
-        MockConnection connection = getJDBCMockObjectFactory().getMockConnection();
-        StatementResultSetHandler statementHandler = connection.getStatementResultSetHandler();
-        MockStatement mockStatement = new MockStatement( connection );
-        Processqueue processqueue = new Processqueue();
 
-        statementHandler.prepareGlobalUpdateCount( 1 ); 
+        statementHandler.prepareGlobalUpdateCount( 1 );
         int testQueueID = 10;
         processqueue.rollback( testQueueID );
 
         String sql_query = String.format( "UPDATE processqueue SET processing = 'N' WHERE queueid = %s", testQueueID );
-        verifySQLStatementExecuted( sql_query );       
+        verifySQLStatementExecuted( sql_query );
         verifyCommitted();
         verifyAllStatementsClosed();
         verifyConnectionClosed();
 
-}
+    }
     public void testRollbackWithInvalidQueueID() throws ConfigurationException, ClassNotFoundException, SQLException{
-        MockConnection connection = getJDBCMockObjectFactory().getMockConnection();
-        StatementResultSetHandler statementHandler = connection.getStatementResultSetHandler();
-        MockStatement mockStatement = new MockStatement( connection );
-        Processqueue processqueue = new Processqueue();
-        
-        statementHandler.prepareGlobalUpdateCount( 0 ); 
+
+        statementHandler.prepareGlobalUpdateCount( 0 );
         int testQueueID = 10;
-        
+
         try{
             processqueue.rollback( testQueueID );
             fail("Should have gotten NoSuchElementException - returned zero rows removed from processqueue table");
@@ -166,14 +150,13 @@ public class ProcessqueueTest extends BasicJDBCTestCaseAdapter {
         catch(NoSuchElementException nse){
             // Expected - intentional
         }
-        
+
         String sql_query = String.format( "UPDATE processqueue SET processing = 'N' WHERE queueid = %s", testQueueID );
-        verifySQLStatementExecuted( sql_query );       
-        
+        verifySQLStatementExecuted( sql_query );
         verifyNotCommitted();
         verifyAllStatementsClosed();
-        verifyConnectionClosed();    
+        verifyConnectionClosed();
     }
 
-    private void TestDeActivate(){}    
+    private void TestDeActivate(){}
 }
