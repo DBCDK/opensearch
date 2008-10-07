098823b88b24964a488af5c3fc2bafa55818ed96
shm <shm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2008-10-07 11:40:56 +0000

    unittest for cargoobjectinfo
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@2581 51cac136-f83d-432d-bb6f-125e83a60a2d

---
+++ src/dbc/opensearch/components/datadock/tests/CargoObjectInfoTest.java
@@ -0,0 +1,25 @@
+package dbc.opensearch.components.datadock.tests;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.io.UnsupportedEncodingException;
+import dbc.opensearch.components.datadock.*;
+
+public class CargoObjectInfoTest{
+
+    CargoMimeType cmt;
+    CargoObjectInfo coi;
+    String test_submitter = "test_submitter";
+    String test_format = "test_format";
+
+    @Before public void SetUp()throws UnsupportedEncodingException{
+        cmt =  CargoMimeType.TEXT_XML;
+        coi = new CargoObjectInfo( cmt, "test_lang", test_submitter,  test_format, 666 );
+    }
+    @Test public void testCorrectnessOfgetSubmitter() {
+        assertTrue( test_submitter.equals( coi.getSubmitter() ) );       
+    }
+    @Test public void testCorrectnessOfgetFormat() {
+        assertTrue( test_format.equals( coi.getFormat() ) );       
+    }
+}
