cd0554633b64308f5dc945d9e8d9853dce4b159d
lvh <lvh@51cac136-f83d-432d-bb6f-125e83a60a2d>
2008-10-16 15:03:14 +0000

    made all the logic into three seperate methods, so that its a bit easier to navigate in
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@2692 51cac136-f83d-432d-bb6f-125e83a60a2d

--- src/dbc/opensearch/components/datadock/DataDockPoolAdm.java
+++ src/dbc/opensearch/components/datadock/DataDockPoolAdm.java
@@ -48,7 +48,9 @@ public class DataDockPoolAdm {
     String[] fileNameList;
     File[] fileList;
     FutureTask[] FTList = null;
-    
+    int answersReceived = 0;
+    int numOfFiles = 0;
+
     public DataDockPoolAdm( Estimate estimate, Processqueue processqueue, FedoraHandler fedoraHandler )throws ConfigurationException, ClassNotFoundException, MalformedURLException, UnknownHostException, ServiceException, IOException {
 
         log.debug( "Entering the constructor" );
@@ -58,14 +60,15 @@ public class DataDockPoolAdm {
         log.debug( "Exiting the constructor" );
     }
     
-    public void start ( String mimetype, String lang, String submitter, String format, String filepath )throws InterruptedException, ExecutionException{
+    public void start ( String mimetype, String lang, String submitter, String format, String filepath )throws FileNotFoundException, InterruptedException, ExecutionException, IOException {
 
         log.debug( String.format( "start the DataDockPool" ) );
+        log.info( String.format( "Creating DataDockPool with %s threads", 10 ) );
 
-        //DataDockPool DDP;
-        //try{
-            log.info( String.format( "Creating DataDockPool with %s threads", 10 ) );
-            DDP = new DataDockPool(10, estimate, processqueue, fedoraHandler );
+        DDP = new DataDockPool(10, estimate, processqueue, fedoraHandler );
+       
+
+    
             
             log.debug( String.format( "Getting properties for the CargoContainer" ) );
             this.mimetype = mimetype;
@@ -74,113 +77,145 @@ public class DataDockPoolAdm {
             this.format = format;
             this.filepath = filepath;
             
-            /** \todo: what the *&^%$ is this? very simple: a unique string to 
-             * match on, this code is NOT intended to be reused! It is a transcription
-             * of a Korean sentence meaning "I do not love you"
-             */
-            String doneString = "narn noRl saRang hATi arNar";
-            String estimateMessageString;
-            int answersReceived = 0;
-
+            // 10: call the reader and filter method
             log.debug( String.format( "read the files defined in the properties into an arraylist (fileList)" ) );
-            // 10: check whether the filepath String is a dir, file or a filetype specification such as "dir/*.xml"
-            // 12: if the filepath is a dir, then get all from that dir
-            if ( new File( filepath ).isDirectory() ){
-                File dir = new File(filepath);
-                // The FileFilter let only files through  that aren't directories 
-                // and if their names doesnt start with "."
-                fileNameList = dir.list( new FileFilter() );
-                fileList = dir.listFiles( new FileFilter() );
+            readFiles();
+            
+            // 20: create the list of FutureTasks
+            
+            createFutureTaskList();
+
+            log.debug( "Calling the checkThreads method" );
+     
+            checkThreads();
+    }
+    
+    /**
+     * Reads the files from the specified path into the array fileList
+     * and the names of the files into the array fileNameList
+     */
+    private void readFiles(){   
+        
+        log.debug( String.format( "read the files defined in the properties into an arraylist (fileList)" ) );
+        
+        // 10: check whether the filepath String is a dir, file or a filetype specification such as "dir/*.xml"
+        // 12: if the filepath is a dir, then get all from that dir
+        if ( new File( filepath ).isDirectory() ){
+            File dir = new File(filepath);
+            // The FileFilter let only files through  that aren't directories 
+            // and if their names doesnt start with "."
+            fileNameList = dir.list( new FileFilter() );
+            fileList = dir.listFiles( new FileFilter() );
+            
+        }else{
+            // 15: if it is a filename get that file
+            if( new File ( filepath ).isFile() ){
+                File file = new File(filepath);
+                fileNameList = new String[]{file.getName()};
+                //fileNameList[0] = file.getName();
+                fileList = new File[]{file}; // and this
+                // fileList[0] = file;
                 
-            }else{
-                // 15: if it is a filename get that file
-                if( new File ( filepath ).isFile() ){
-                    File file = new File(filepath);
-                    fileNameList = new String[1]; //there must be a better way to do this
-                    fileNameList[0] = file.getName();
-                    fileList = new File[1]; // and this
-                    fileList[0] = file;
-                    
-                }else{//we assume its a path ending with *.xml signaling that we must take 
-                    // all .xml files in the directory
-                    log.debug(String.format("Extracting from the dirpath form index '%s' to index '%s'", 0, filepath.lastIndexOf('/') ));
-
-
-                    String dirpathString = filepath.substring( 0, filepath.lastIndexOf('/') );
-                    log.debug(String.format("dirpathString = '%s'", dirpathString) );
-                    File dirpath = new File(dirpathString);
-                    fileNameList = dirpath.list( new XmlFileFilter() );
-                    fileList = dirpath.listFiles( new XmlFileFilter() );
-                    
-                }                        
-            }
+            }else{//we assume its a path ending with *.xml signaling that we must take 
+                // all .xml files in the directory
+                log.debug(String.format("Extracting from the dirpath form index '%s' to index '%s'", 0, filepath.lastIndexOf('/') ));
+                
+                String dirpathString = filepath.substring( 0, filepath.lastIndexOf('/') );
+                log.debug(String.format("dirpathString = '%s'", dirpathString) );
+                File dirpath = new File(dirpathString);
+                fileNameList = dirpath.list( new XmlFileFilter() );
+                fileList = dirpath.listFiles( new XmlFileFilter() );
+                
+            }                        
+        }
+        
+    }
+    
+    /**
+    * creates the cargoContainers and gives them to the createAndJoinThread on the DDP
+    * the returned FutureTask are put on the FTList
+    */
+    private void createFutureTaskList() throws FileNotFoundException, IOException {
+        
+        log.debug( String.format( "check if we got any files from the filepath" ) );
+        if( fileList == null ){
+            throw new IllegalArgumentException( String.format( "no files on specified path: %s", filepath ) );
+        }
+        
+        numOfFiles = fileList.length;
+        log.info(String.format( "\n number of files = %s \n", numOfFiles ) );
+        FTList = new FutureTask[ numOfFiles ];
+        
+        InputStream data;
+        
+        log.debug( String.format( "create CargoContainers and give them to the DDP.createAndJoinThread method" ) );
+        log.debug( String.format( "store the FutureTask in an array together with documenttitle" ) );
+        
+        for(int filesSent = 0; filesSent < numOfFiles; filesSent++){
             
-            log.debug( String.format( "check if we got any files from the filepath" ) );
-            if( fileList == null ){
-                throw new IllegalArgumentException( String.format( "no files on specified path: %s", filepath ) );
-            }
-            
-            int numOfFiles = fileList.length;
-            log.info(String.format( "\n number of files = %s \n", numOfFiles ) );
-            FTList = new FutureTask[ numOfFiles ];
-            
-            InputStream data;
-            
-            log.debug( String.format( "create CargoContainers and give them to the DDP.createAndJoinThread method" ) );
-            log.debug( String.format( "store the FutureTask in an array together with documenttitle" ) );
-            
-            for(int filesSent = 0; filesSent < numOfFiles; filesSent++){
+                data = new FileInputStream(fileList[filesSent]);
+                CargoContainer cc = new CargoContainer(data, mimetype, lang, submitter, format);
                 try{
-                    data = new FileInputStream(fileList[filesSent]);
-                    CargoContainer cc = new CargoContainer(data, mimetype, lang, submitter, format);
                     FTList[filesSent] = DDP.createAndJoinThread(cc);
                     log.info( String.format( "Calling createAndJoin %s. time", filesSent + 1 ) );
                     
-                }catch(Exception thise){
-                    System.out.print("\n Exception in DataDockPoolAdm \n");
-                    thise.printStackTrace();
-                    System.exit(1);
+                }catch( ConfigurationException ce ){
+                
+                }catch( ClassNotFoundException cnfe ){
                 }
-            }
-            log.info( "All files given to the DataDockPool" );
-            //Loop that continues until all files have docked
-            log.info("entering while loop in DataDockPoolAdm");
-           
-            while(answersReceived < numOfFiles){
-                //         log.info("\n In the while loop \n");
-                for(int x = 0; x < numOfFiles; x++){
-                    //check if answer is received for this file
-                    if(fileNameList[x].equals( doneString) ){
-                        // go to next element
-                    }else{
-                        //log.info( "\n calling isDone \n" );
-                        if( FTList[x].isDone() ){
-                            try{
-                                // log.debug( "A thread is done or it terminated with an exception" );
-                                estimateMessageString = String.format("The file: %s , will take approximately: %s to process \n", fileNameList[x],FTList[x].get() );
-                                log.info(estimateMessageString);
-                            }catch(ExecutionException ee){
-                                //Catching exception from the thread
-                                Throwable cause = ee.getCause();
-                                log.fatal( String.format( "Caught thread error associated with file: '%s' ", fileNameList[x] ) );
-                                RuntimeException re = new RuntimeException(cause);
-                                throw re;
-                            }
+        }
+        log.info( "All files given to the DataDockPool" );
+    }
 
-                            /**
-                             * Changing the corresponding String in the nameList 
-                             * to the donestring, yes its crappy, but it works for now 
-                             */                          
-                            fileNameList[x] = doneString; 
-                            answersReceived++;   
-                            log.info(String.format("%s files to go",numOfFiles - answersReceived));
-                            
+    /**
+     * checks the FutureTask whether they are done or running. When a FututreTask
+     *  is done the result is "given to the user"
+     */ 
+    private void checkThreads() throws InterruptedException {
+        log.info( "entering while loop in DataDockPoolAdm" );
+        
+        /** \todo: what the *&^%$ is this? very simple: a unique string to 
+         * match on, this code is NOT intended to be reused! It is a transcription
+         * of a Korean sentence meaning "I do not love you"
+         */
+        String doneString = "narn noRl saRang hATi arNar";
+        
+        String estimateMessageString;
+
+        while(answersReceived < numOfFiles){
+            //         log.info("\n In the while loop \n");
+            for(int x = 0; x < numOfFiles; x++){
+                //check if answer is received for this file
+                if(fileNameList[x].equals( doneString) ){
+                    // go to next element
+                }else{
+                    //log.info( "\n calling isDone \n" );
+                    if( FTList[x].isDone() ){
+                        try{
+                            // log.debug( "A thread is done or it terminated with an exception" );
+                            estimateMessageString = String.format("The file: %s , will take approximately: %s to process \n", fileNameList[x],FTList[x].get() );
+                            log.info( estimateMessageString );
+                        }catch(ExecutionException ee){
+                            //Catching exception from the thread
+                            Throwable cause = ee.getCause();
+                            log.fatal( String.format( "Caught thread error associated with file: '%s' ", fileNameList[x] ) );
+                            RuntimeException re = new RuntimeException(cause);
+                            throw re;
                         }
+                        
+                        /**
+                         * Changing the corresponding String in the nameList 
+                         * to the donestring, yes its crappy, but it works for now 
+                         */                          
+                        fileNameList[x] = doneString; 
+                        answersReceived++;   
+                        log.info(String.format("%s files to go",numOfFiles - answersReceived));
+                        
                     }
-                    
                 }
+                
             }
-            log.info( String.format( "%s files stored in Fedora", numOfFiles ) );
-            
+        }
+        log.info( String.format( "%s files stored in Fedora", numOfFiles ) );
     }
 }
