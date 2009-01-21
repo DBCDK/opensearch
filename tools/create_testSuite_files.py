a52d5329512b672f8aa03e0d51e4929e97dcfbbc
mro <mro@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-01-21 14:43:54 +0000

    Adding script for generating test suite files
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@3038 51cac136-f83d-432d-bb6f-125e83a60a2d

---
+++ tools/create_testSuite_files.py
@@ -0,0 +1,77 @@
+#! /usr/bin/python
+
+import sys
+import os
+import fnmatch
+
+
+def writeTextToFile( dir, className, fileTxt ):
+    fileName = dir + '/' + className + '.java'
+    f = open( fileName, 'w')
+    f.write(fileTxt)
+    f.close()
+
+
+
+def createTestSuiteFileText( dir, className, testNames ):
+    fileTxt = str()
+    tmp = str()
+    
+    # Write package name
+    tmp = dir[dir.index( 'dk' ):]
+    tmp = tmp.replace( '/', '.' )
+
+    doubleNL = '\n\n'
+    fileTxt += 'package ' + (tmp) + ';' + doubleNL
+
+    # Write annotation
+    fileTxt += 'import org.junit.runner.RunWith;\n'
+    fileTxt += 'import org.junit.runners.Suite;\n' + doubleNL
+    fileTxt += '@RunWith(Suite.class)' + doubleNL
+    fileTxt += '@Suite.SuiteClasses(' + '\n    {\n'
+
+    """    for test in testNames:        
+        fileTxt += '\t' + test + '.class,\n'"""
+    while len(testNames) > 1:
+        fileTxt += '\t\t' + testNames.pop() + '.class,\n'
+        
+    fileTxt += '\t\t' + testNames.pop() + '.class\n'
+
+    fileTxt += '    }\n)\n'
+
+    # Write class
+    fileTxt += 'public class ' + className + '\n{\n'
+    fileTxt += '\t// Leave class empty!\n}\n'
+
+    return fileTxt
+
+
+
+def match( ptr, dir, names ):
+    tests = list()
+
+    fileTxt = str()
+    if fnmatch.fnmatch( dir, '*tests' ):
+        for name in names:
+            if name.endswith( ptr ):
+                tests.append( name.replace( '.java', '' ) )
+        if len( tests ) > 0:
+            projectName = str()
+            if dir.endswith('/tests'):
+                projectName = dir.replace( '/tests', '' ).split('/').pop()
+                className = projectName.upper() + 'TestSuite'
+                fileTxt = createTestSuiteFileText( dir, className, tests )
+                writeTextToFile( dir, className, fileTxt )
+
+    
+    
+sourceDir = os.getcwd()
+if sourceDir.endswith( 'tools'):
+     sourceDir = '../src/dk/dbc/opensearch'
+     os.path.walk(sourceDir, match, 'Test.java')
+else:
+    print '<ERROR>'
+    print 'Curr Dir should be ./.../opensearch/tools'
+
+
+
