8426e477720c609b0aab3842bb14a1e420c733b3
lvh <lvh@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-01-23 06:02:47 +0000

    the pluginFramework wrapper class
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@3064 51cac136-f83d-432d-bb6f-125e83a60a2d

---
+++ src/dk/dbc/opensearch/common/pluginframework/PluginFramework.java
@@ -0,0 +1,30 @@
+package dk.dbc.opensearch.common.pluginframework;
+
+import javax.xml.parsers.DocumentBuilder;
+import org.apache.log4j.Logger;
+
+import java.lang.IllegalArgumentException;
+import java.io.FileNotFoundException;
+import java.lang.InstantiationException;
+import java.lang.IllegalAccessException;
+/**
+ * 
+ */
+public class PluginFramework {
+    private PluginFinder PFinder;
+    private PluginLoader PLoader;
+
+  /**
+   * 
+   */
+    public PluginFramework( DocumentBuilder docBuilder, ClassLoader pluginClassLoader )throws NullPointerException, FileNotFoundException {
+        PFinder = new PluginFinder( docBuilder );
+        PLoader = new PluginLoader( pluginClassLoader );
+    
+  }
+    public IPluggable getPlugin( String submitter, String format, String task )throws IllegalArgumentException, FileNotFoundException, InstantiationException, IllegalAccessException{
+        String key = submitter + format + task;
+        String pluginClassName = PFinder.getPluginClassName( key );
+        return PLoader.getPlugin( pluginClassName );
+    }
+}
