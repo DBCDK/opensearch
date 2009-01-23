e0c478b256006f014e271e435da968560424f87e
lvh <lvh@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-01-23 06:06:22 +0000

    Filter for .plugin files
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@3066 51cac136-f83d-432d-bb6f-125e83a60a2d

---
+++ src/dk/dbc/opensearch/common/os/PluginFileFilter.java
@@ -0,0 +1,32 @@
+/**
+ * \file PluginFileFilter.java
+ * \brief FilenameFilter class implementation that filters plugin files
+ * \package tools
+ */
+package  dk.dbc.opensearch.common.os;
+
+import java.io.File;
+import java.io.FilenameFilter;
+/**
+ * \ingroup os
+ * \brief Filter extract .plugin files
+ */
+public class PluginFileFilter implements FilenameFilter{
+    
+    /**
+     *
+     * @param dir the path of the directory to be tested
+     * @param name the dir- or filename on the path
+     *
+     * @returns true if path denotes a file that ends with ".plugin"
+     *
+     * @throws NullPointerException if the dir- or filename is null
+     */
+    public boolean accept(File dir, String name) throws NullPointerException{
+        if( !(new File(name).isDirectory()) && name.endsWith( ".plugin" ) ){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
