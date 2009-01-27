7dd1448baf68e27bb3e4f4b1cb17d6dd698239ba
stm <stm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-01-27 07:40:21 +0000

    overloaded exception constructor
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@3096 51cac136-f83d-432d-bb6f-125e83a60a2d

--- src/dk/dbc/opensearch/common/pluginframework/PluginResolverException.java
+++ src/dk/dbc/opensearch/common/pluginframework/PluginResolverException.java
@@ -4,18 +4,24 @@ import java.util.Vector;
 import com.mallardsoft.tuple.Tuple;
 import com.mallardsoft.tuple.Pair;
 /**
- * 
+ *
  */
 public class PluginResolverException extends Exception {
     Vector<Pair<Throwable, String>> exceptionVector;
     String message;
-  /**
-   * 
-   */
-    public PluginResolverException( Vector<Pair<Throwable, String>> exceptoinVector, String message) {
-      this.exceptionVector = exceptionVector;
-      this.message = message;
-  }
+
+    /**
+     *
+     */
+    public PluginResolverException( Vector<Pair<Throwable, String>> exceptionVector, String message ) {
+        this.exceptionVector = exceptionVector;
+        this.message = message;
+    }
+
+    public PluginResolverException( String message ) {
+        this.exceptionVector = null;
+        this.message = message;
+    }
     public String getMessage(){
         return message;
     }
