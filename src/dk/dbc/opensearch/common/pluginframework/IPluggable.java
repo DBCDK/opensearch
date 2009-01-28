a3a724537e5e997a9c63be19c785149e4902b7d9
stm <stm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-01-28 12:50:23 +0000

    Description of the IPluggable purpose and usage
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@3122 51cac136-f83d-432d-bb6f-125e83a60a2d

--- src/dk/dbc/opensearch/common/pluginframework/IPluggable.java
+++ src/dk/dbc/opensearch/common/pluginframework/IPluggable.java
@@ -1,14 +1,38 @@
 package dk.dbc.opensearch.common.pluginframework;
-
 /**
- * This interface represents the base point for all plugins. The only
- * method supported by the interface is the identification of the
- * plugin via the pluginid
+ * This interface represents the base point for all plugins. It
+ * defines two methods that are the common basis for all plugins that
+ * perform tasks in the datadock or pti.
+ * 
+ * The pluginID must be given to the plugin at object creation
+ * time. The taskname is defined at compile time but could be the
+ * subject of configuration variables in the future.
+ * 
+ * \todo: make the taskname a configuration value instead of setting it compile time
  */
 public interface IPluggable
 {
 
+    /**
+     * getPluginID returns the - at object creation time given - id
+     * for this plugin. pluginIDs identify plugins to the
+     * pluginframework, which exposes the plugin to the system based
+     * on which task and format the plugin can handle and which
+     * submitters the plugin is valid for.
+     * 
+     * @returns a string containing the plugin id
+     */
     String getPluginID();
+
+    /**
+     * getTaskName returns the - at compile time given - name of the
+     * task the plugin is constructed to handle. The task name is used
+     * by the users of the plugin framework to assign jobs and
+     * jobqueues to the plugins involved in getting a job through the
+     * system.
+     * 
+     * @returns a string containing the task name
+     */
 
     String getTaskName();
 }
