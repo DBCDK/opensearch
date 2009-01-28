17defaa6c362d1ffddd48f2e52cf5501dcb2498d
stm <stm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-01-28 16:33:31 +0000

    IPluggable now contains framework documentation
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@3124 51cac136-f83d-432d-bb6f-125e83a60a2d

--- src/dk/dbc/opensearch/common/pluginframework/IPluggable.java
+++ src/dk/dbc/opensearch/common/pluginframework/IPluggable.java
@@ -9,6 +9,27 @@ package dk.dbc.opensearch.common.pluginframework;
  * subject of configuration variables in the future.
  * 
  * \todo: make the taskname a configuration value instead of setting it compile time
+ * 
+ * Plugins in the opensearch plugin framework are expected to work
+ * primarily with CargoContainers. Plugins are defined on the basis of
+ * the format they're going to work with combined with th e type of
+ * task they're going to perform. E.g. for the format 'faktalink' (DBC
+ * material, formatted in XML), the tasks could likely be:
+ * 
+ * 1) harvest: reading one or more files from a path and putting the
+ *    content of the files and known metadata about them into
+ *    CargoContainers.
+ *    
+ * 2) annotate: lookup metadata relating to the data in a
+ *    CargoContainer and annotate the metadata to the
+ *    CargoContainer(s).
+ *    
+ * 3) index: convert the CargoContainer into a lucene index
+ * 
+ * 4) store: format the CargoContainer into a Fedora Digital Object
+ *    and store the digital object in the Fedora Digital Repository.
+ * 
+ * 
  */
 public interface IPluggable
 {
