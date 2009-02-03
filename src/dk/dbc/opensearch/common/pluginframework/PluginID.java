11629c450d087e7bedddc45ed9f5f5aa15d498c0
stm <stm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-02-03 11:34:42 +0000

    the PluginID class now has documentation
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@3161 51cac136-f83d-432d-bb6f-125e83a60a2d

--- src/dk/dbc/opensearch/common/pluginframework/PluginID.java
+++ src/dk/dbc/opensearch/common/pluginframework/PluginID.java
@@ -15,7 +15,9 @@ public class PluginID {
     private String task;
 
     /**
-     *
+     * @param submitter Information on the submitter of the material. The submitter should be known by the system through other means than registering with a plugin
+     * @param format Information on the format of the submitted material. 
+     * @param task Information on the task that the plugin handles. This information is matched a String in the plugins 
      */
     public PluginID( String submitter, String format, String task ) {
         this.submitter = submitter;
@@ -23,17 +25,37 @@ public class PluginID {
         this.task = task;
     }
 
+    /**
+     * getPluginID returns a hashvalue based on the submitter, format
+     * and task that the PluginID object is constructed with. Please
+     * note that the hashvalue is dependant on the (in this method
+     * embedded) position of the informations.
+     * 
+     * @returns an integer defining the hash value of the plugin.
+     */
     public int getPluginID(){
         String hashSubject = submitter+format+task;
         return hashSubject.hashCode();
     }
 
+    /**
+     * @returns the value of the submitter associated with the
+     * pluginid
+     */
     public String getPluginSubmitter(){
         return submitter;
     }
+    /**
+     * @returns the value of the format associated with the
+     * pluginid
+     */
     public String getPluginFormat(){
         return format;
     }
+    /**
+     * @returns the value of the task associated with the
+     * pluginid
+     */
     public String getPluginTask(){
         return task;
     }
