6a5b0458eaad03f6a9b022a71065fd3502d264e7
stm <stm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-02-04 14:36:37 +0000

    adding simple handling of pair types
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@3175 51cac136-f83d-432d-bb6f-125e83a60a2d

---
+++ src/dk/dbc/opensearch/common/types/Pair.java
@@ -0,0 +1,24 @@
+/**
+ * InputPair
+ */
+public class Pair<E, V> {
+    /**
+     * 
+     */
+
+    private E first;
+    private V second;
+
+    public Pair( E first, V second ) {
+        this.first = first;
+        this.second = second;     
+    }
+
+    public E getFirst(){
+        return first;
+    }
+
+    public V getSecond(){
+        return second;
+    }
+}
