5b4c3f0ff8e7d7f7fb196affdae308244cd46dc3
stm <stm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-02-12 22:31:49 +0000

    rev 3265 could not build because these files were missing. It is now 11:30 pm which should be adequate explanation
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@3267 51cac136-f83d-432d-bb6f-125e83a60a2d

---
+++ src/dk/dbc/opensearch/plugins/FaktalinkRetriever.java
@@ -0,0 +1,108 @@
+/**
+ * 
+ */
+package dk.dbc.opensearch.plugins;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.rmi.RemoteException;
+import java.util.NoSuchElementException;
+
+import javax.xml.rpc.ServiceException;
+
+import org.apache.log4j.Logger;
+
+import dk.dbc.opensearch.common.pluginframework.IPluggable;
+import dk.dbc.opensearch.common.types.CargoContainer;
+
+import info.fedora.www.definitions._1._0.types.DatastreamDef;
+import info.fedora.www.definitions._1._0.types.MIMETypedStream;
+
+/**
+ * @author stm
+ *
+ */
+public class FaktalinkRetriever extends FedoraHandle implements IPluggable {
+
+	public FaktalinkRetriever() throws ServiceException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	Logger log = Logger.getLogger( this.getClass() );
+	
+    /**
+     * \brief creates a cargocontainer by getting a dataobject from the repository, identified by the parameters.
+     * \todo: what are these parameters?
+     *
+     * @param pid 
+     * @param itemId
+     *
+     * @returns The cargocontainer constructed
+     *
+     * @throws IOException something went wrong initializing the fedora client
+     * @throws NoSuchElementException if there is no matching element on the queue to pop
+     * @throws RemoteException error in communiction with fedora
+     * @throws IllegalStateException pid mismatch when trying to write to fedora
+     */    
+    public CargoContainer getDatastream( String pid, String itemId ) throws IOException, NoSuchElementException, RemoteException, IllegalStateException
+    {
+        log.debug( String.format( "getDatastream( pid=%s, itemId=%s ) called", pid, itemId ) );
+       
+        String pidNS = pid.substring( 0, pid.indexOf( ":" ));
+       
+        /** \todo: very hardcoded value */
+        String itemId_version = itemId+".0";
+        
+        CargoContainer cargo = null;
+        DatastreamDef[] datastreams = null;
+        MIMETypedStream ds = null;
+
+        log.debug( String.format( "Retrieving datastream information for PID %s", pid ) );
+        
+        datastreams = super.fea.listDatastreams( pid, null );
+        
+        log.debug( String.format( "Iterating datastreams" ) );
+        
+        for ( DatastreamDef def : datastreams )
+        {
+            log.debug( String.format( "Got DatastreamDef with id=%s", def.getID() ) );
+            
+            if( def.getID().equals( itemId ) )
+            {                
+                log.debug( String.format( "trying to retrieve datastream with pid='%s' and itemId_version='%s'", pid, itemId ) );
+                ds = super.fea.getDatastreamDissemination( pid, itemId, null );
+                //ds = apia.getDatastreamDissemination( pid, itemId, null );
+                // pid and def.getID() are equal, why give them both?
+ 
+                log.debug( String.format( "Making a bytearray of the datastream" ) );
+                byte[] datastr = ds.getStream();
+
+                log.debug( String.format( "Preparing the datastream for the CargoContainer" ) );
+                InputStream inputStream = new ByteArrayInputStream( datastr );
+
+                log.debug( String.format( "DataStream ID      =%s", itemId ) );
+                log.debug( String.format( "DataStream Label   =%s", def.getLabel() ) );
+                log.debug( String.format( "DataStream MIMEType=%s", def.getMIMEType() ) );
+
+                // dc:format holds mimetype as well
+                /** \todo: need to get language dc:language */
+                String language = "";
+
+                cargo = new CargoContainer();
+                cargo.add( itemId, pidNS, language, def.getMIMEType(), inputStream);
+                //cargo = new CargoContainer( inputStream, def.getMIMEType(),language,pidNS,itemId );
+            }
+        }
+        
+        if( cargo == null )
+        {
+            throw new IllegalStateException( String.format( "no cargocontainer with data matching the itemId '%s' in pid '%s' ", itemId, pid ) );
+        }
+
+        log.debug( String.format( "CargoContainer.mimetype =     %s", cargo.getItemsCount() ) );
+        log.info( "Successfully retrieved datastream." );
+        return cargo;
+    }
+}
---
+++ src/dk/dbc/opensearch/plugins/FaktalinkStore.java
@@ -0,0 +1,90 @@
+/**
+ *
+ */
+package dk.dbc.opensearch.plugins;
+
+import java.io.IOException;
+
+import java.text.ParseException;
+
+import javax.xml.rpc.ServiceException;
+
+import org.apache.axis.types.NonNegativeInteger;
+import org.apache.log4j.Logger;
+import org.exolab.castor.xml.MarshalException;
+import org.exolab.castor.xml.ValidationException;
+
+import dk.dbc.opensearch.common.types.CargoContainer;
+
+import dk.dbc.opensearch.common.fedora.FedoraTools;
+import dk.dbc.opensearch.common.pluginframework.IPluggable;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author stm
+ *
+ * This plugin handles the Datadock communication with the fedora
+ * base. The CargoContainers contents is stored in a DigitalObject
+ * object and saved to the fedorabase
+ */
+public class FaktalinkStore extends FedoraHandle implements IPluggable {
+
+    public FaktalinkStore() throws ServiceException {
+		super();
+	}
+
+	Logger log = Logger.getLogger( "FaktaLinkStore" );
+	
+    private CargoContainer cargo;
+
+    /*
+     * Initialises the plugin with a cargocontainer.
+     */
+    public void init( CargoContainer cargo ) throws ServiceException {
+        this.cargo = cargo;
+    }
+
+    public float storeData( ) throws MarshalException, ValidationException, ServiceException, IOException, ParseException{
+
+    	String pid = this.storeCargo();
+
+    	this.putPidOnQueue( pid );
+    	
+    	return 0;
+
+
+    }
+
+    private String storeCargo( )throws ServiceException, MarshalException, ValidationException, IOException, ParseException, IllegalStateException
+    {
+        NonNegativeInteger nni = new NonNegativeInteger( "1" );
+        String[] pids = super.fem.getNextPID( nni, "" );
+
+        String descriptive_label = String.format( "Faktalink" );
+        
+        byte[] foxml = FedoraTools.constructFoxml(this.cargo, pids[0], descriptive_label );
+        
+        String logm = String.format( "Faktalink inserted" );
+        
+        String pid = super.fem.ingest( foxml, "Faktalink", logm);
+
+        /** \todo: We need a pid-manager for getting lists of available pids for a given ns 
+         *  this whole getNextPID vs. ingest return type looks dubious. I fear it will break in a multithreaded environment
+         */
+        if( ! pid.equals( pids[0] ) ){
+            log.fatal( String.format( "we expected pid=%s, but got pid=%s", pids[0], pid ) );
+            throw new IllegalStateException( String.format( "expected pid=%s, but got pid=%s", pids[0], pid ) );
+        }
+
+        log.info( String.format( "Submitted data, returning pid %s", pid ) );
+        
+        return pid;
+    }
+
+    private void putPidOnQueue( String pid ){
+
+
+    }
+
+}
