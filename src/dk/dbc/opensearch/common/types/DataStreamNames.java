9a958fcfa5a609d0701d74d100a2427db0bedd07
mro <mro@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-02-16 14:26:23 +0000

    Forgot the DataStreamNames file.
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@3295 51cac136-f83d-432d-bb6f-125e83a60a2d

---
+++ src/dk/dbc/opensearch/common/types/DataStreamNames.java
@@ -0,0 +1,64 @@
+package dk.dbc.opensearch.common.types;
+
+import org.apache.log4j.Logger;
+
+public enum DataStreamNames 
+{	
+	OriginalData ( "originalData", "original data" ),
+	DublinCoreData( "dublinCoreData", "dublin core data" );
+	
+	static Logger log = Logger.getLogger( DataStreamNames.class );
+	
+	String name;
+	String description;
+	
+	
+	DataStreamNames( String name, String description ) 
+	{		
+		this.name = name;
+		this.description = description;
+	}
+	
+	
+	public String getName()
+	{
+		return this.name;
+	}
+	
+	
+	public String getDescription()
+	{
+		return this.description;
+	}
+
+
+	public static boolean validDataStreamNameType( String nametype )
+    {
+        DataStreamNames DSN = DataStreamNames.getDataStreamNameFrom( nametype );
+        log.debug( "checking dataStreamName" );
+        
+        if( DSN == null )
+        	return false;
+        
+        return true;
+    }
+	
+	
+    /**
+     * @param mime
+     * @return
+     */
+    public static DataStreamNames getDataStreamNameFrom( String name )
+    {
+        DataStreamNames DSN = null;
+        for (DataStreamNames dsn : DataStreamNames.values() )
+        {
+            if( name.equals( dsn.getName() ) )
+            {
+                DSN = dsn;
+            }
+        }
+        
+        return DSN;
+    }   
+}
