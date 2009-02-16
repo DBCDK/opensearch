20456ecd660b5eae003926c0e48367af790f6065
lvh <lvh@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-02-16 17:14:04 +0000

    added the JobMapCreator class added equals, toString and hashCode methods to the Pair class
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@3297 51cac136-f83d-432d-bb6f-125e83a60a2d

---
+++ src/dk/dbc/opensearch/common/helpers/JobMapCreator.java
@@ -0,0 +1,153 @@
+package dk.dbc.opensearch.common.helpers;
+
+import dk.dbc.opensearch.common.os.FileHandler;
+import dk.dbc.opensearch.common.types.Pair;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Vector;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Collections;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import org.apache.log4j.Logger;
+
+/**
+ *
+ */
+public class JobMapCreator {
+
+    static Logger log = Logger.getLogger( "JobMapCreator" );
+    private HashMap< Pair< String, String >, ArrayList<String> > jobMap;
+    //    private HashMap< Pair< String, String >, List<String> > DDJobMap;
+    // \Todo: Beware hardcoded values
+    private String datadockJobsPath = "config/datadock_jobs.xml";
+    private String ptiJobsPath = "/config/pti_jobs.xml";
+    private File jobFile;
+    private Document jobDocument;
+
+    DocumentBuilderFactory docBuildFact;
+    DocumentBuilder docBuilder;
+
+    /**
+     * a little helper for comparing the int thats is second memeber in Pairs
+     * used in making the list with the tasks sorted due to posistion
+     */
+    class secondComparator implements Comparator{
+
+        public int compare( Object x, Object y ){
+
+            if( ((Pair<String, Integer>)x).getSecond() < ((Pair<String, Integer>)y).getSecond() ){
+                return -4;
+            }else{
+                if( ((Pair<String, Integer>)x).getSecond() == ((Pair<String, Integer>)y).getSecond() ){
+                    return 0;
+                }
+            }
+            return 4;
+        }
+
+    }
+
+    secondComparator secComp = new secondComparator();
+
+    /**
+     *
+     */
+    public JobMapCreator( Class classType )throws ParserConfigurationException, SAXException, IOException {
+
+        docBuildFact = DocumentBuilderFactory.newInstance();
+        docBuilder = docBuildFact.newDocumentBuilder();
+
+        jobMap = new HashMap();
+
+        if( classType.getName().equals( "dk.dbc.opensearch.components.datadock.DatadockMain") ){
+            jobFile = FileHandler.getFile( datadockJobsPath );
+        }else{
+            jobFile = FileHandler.getFile( ptiJobsPath );
+        }
+
+        jobDocument = docBuilder.parse( jobFile );
+
+        //build the jobMap
+        // 10: get the rootelement of the doc
+        Element xmlRoot = jobDocument.getDocumentElement();
+
+        // 20: Get the NodeList
+        NodeList jobNodeList = xmlRoot.getElementsByTagName( "job" );
+
+        String submitter;
+        String format;
+        ArrayList<String> sortedTaskList = new ArrayList();
+        List< Pair< String, Integer > > taskAndPriority = new ArrayList();
+        Element jobElement;
+        NodeList taskList;
+        int taskListLength;
+        Element taskElement;
+        Pair taskPriPair;
+        String task;
+        int position;
+        int listLength = jobNodeList.getLength();
+
+        // 30: For each node read the task name and position
+        for( int x = 0; x < listLength ; x++ ){
+
+            jobElement = (Element)jobNodeList.item( x );
+
+            submitter = jobElement.getAttribute( "submitter" );
+            format = jobElement.getAttribute( "format" );
+
+            taskList = jobElement.getElementsByTagName( "task" );
+            taskListLength = taskList.getLength();
+
+            taskAndPriority.clear();
+
+            // 35: get the tasks in a List
+            for( int y = 0; y < taskListLength; y++ ){
+
+                taskElement = (Element)taskList.item( y );
+                //get the name and position of the task element
+                task = (String)taskElement.getAttribute( "name" );
+                position = Integer.decode(taskElement.getAttribute( "position" ) );
+
+
+
+                taskAndPriority.add( new Pair( task, position ) );
+            }
+
+            // 40: sort the tasks based on the position (order)
+            Collections.sort( taskAndPriority, secComp );
+
+            // 50: put it in a List
+            sortedTaskList.clear();
+            for( int z = 0; z < taskListLength; z++ ){
+                task = ( (Pair< String, Integer >)taskAndPriority.get( z ) ).getFirst();
+                sortedTaskList.add( task );
+            }
+            // 60: Put it into the map with  <submitter, format> as key and List as value
+
+            jobMap.put( new Pair< String, String >( submitter, format ), new ArrayList(sortedTaskList) );
+        }
+    }
+
+    public HashMap< Pair< String, String >, ArrayList< String > > getMap(){
+
+        return jobMap;
+    }
+}
--- src/dk/dbc/opensearch/common/types/Pair.java
+++ src/dk/dbc/opensearch/common/types/Pair.java
@@ -2,10 +2,11 @@ package dk.dbc.opensearch.common.types;
 
 /**
  * InputPair
+ * not for use when values are to be compared until an equals() method is implemented
  */
 public class Pair<E, V> {
     /**
-     * 
+     *
      */
 
     private E first;
@@ -13,7 +14,7 @@ public class Pair<E, V> {
 
     public Pair( E first, V second ) {
         this.first = first;
-        this.second = second;     
+        this.second = second;
     }
 
     public E getFirst(){
@@ -23,4 +24,23 @@ public class Pair<E, V> {
     public V getSecond(){
         return second;
     }
+    @Override public boolean equals( Object obj){
+        if(!( obj instanceof Pair ) ){
+            return false;
+        }
+        if(!( first.equals( ( (Pair)obj ).getFirst() ) ) ){
+            return false;
+        }
+        if(!( second.equals( ( (Pair)obj ).getSecond() ) ) ){
+            return false;
+        }
+        return true;
+    }
+    @Override public String toString(){
+        return first.toString() + ", " + second.toString();
+    }
+    @Override public int hashCode(){
+        return first.hashCode() + second.hashCode();
+    }
+
 }
