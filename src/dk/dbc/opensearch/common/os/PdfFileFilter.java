bc51daece9a044c871350d7f040a05c37c001655
shm <shm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-02-19 14:45:13 +0000

    added rub harvester
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@3333 51cac136-f83d-432d-bb6f-125e83a60a2d

---
+++ src/dk/dbc/opensearch/common/os/PdfFileFilter.java
@@ -0,0 +1,35 @@
+/**
+ * \file PdfFileFilter.java
+ * \brief The PdfFileFilter class
+ * \package os;
+ */
+
+package dk.dbc.opensearch.common.os;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+/**
+ * 
+ */
+public class PdfFileFilter implements FilenameFilter{
+    /**
+     * This method returns true only if arguments dir+name evals to a
+     * filename not starting with a '.' and the suffix is .pdf
+     *
+     * @param dir the path of the directory to be tested
+     * @param name the dir- or filename on the path
+     *
+     * @returns true if path denotes a file that ends with ".pdf"
+     *
+     * @throws NullPointerException if the dir- or filename is null
+     */
+    public boolean accept(File dir, String name) throws NullPointerException{
+
+        if( ! ( new File( name ) ).isDirectory() && name.endsWith( ".pdf" ) ){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
