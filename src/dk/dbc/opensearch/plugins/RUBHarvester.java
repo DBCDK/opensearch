a82a919359d669fecd135edc40b6b3ddd8bf6864
shm <shm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-02-22 15:59:54 +0000

    the finished? harvester
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@3363 51cac136-f83d-432d-bb6f-125e83a60a2d

--- src/dk/dbc/opensearch/plugins/RUBHarvester.java
+++ src/dk/dbc/opensearch/plugins/RUBHarvester.java
@@ -11,20 +11,32 @@ import dk.dbc.opensearch.common.types.CargoContainer;
 import dk.dbc.opensearch.common.types.DataStreamNames;
 import dk.dbc.opensearch.common.types.DatadockJob;
 import dk.dbc.opensearch.common.pluginframework.IHarvestable;
+import dk.dbc.opensearch.common.types.CargoObject;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import java.io.File;
 import java.io.FilenameFilter;
 import dk.dbc.opensearch.common.os.PdfFileFilter;
 import dk.dbc.opensearch.common.os.XmlFileFilter;
 import dk.dbc.opensearch.common.os.FileHandler;
-import dk.dbc.opensearch.common.os.StreamHandler;
 
 import java.util.Vector;
 
 import org.apache.log4j.Logger;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+
 
 /**
  * Plugin class for harvesting RUB data.
@@ -57,29 +69,53 @@ public class RUBHarvester implements IHarvestable{
      * @return the CargoContainer
      * @throws IOException if the data cannot be read
      */
-    public CargoContainer getCargoContainer() throws IOException 
+    public CargoContainer getCargoContainer() throws IOException, ParserConfigurationException, SAXException, javax.xml.xpath.XPathExpressionException
     { 
         log.debug( "getCargoContainer() called" );
 
         CargoContainer cargoContainer = new CargoContainer();
-
         String mimetype = "application/pdf";
-        String lang = "EN"; // can we test the pdf doc for this?
-        
-        // read and add pdf to cargoContainer
+
+        log.debug( "read and add xml to cargoContainer" );
+       
+        FilenameFilter[] xmlFilter = { new XmlFileFilter() };
+        String xmlFilePath = FileHandler.getFileList( path, xmlFilter, false ).remove(0);        
+        log.debug( String.format( "xml filepath='%s'", xmlFilePath ) );
+        File xmlFile = FileHandler.getFile( xmlFilePath );
+        FileInputStream xmlData = FileHandler.readFile( xmlFilePath ); 
+
+        byte xmlBytes[] = new byte[(int)xmlFile.length()];
+        xmlData.read(xmlBytes);
+
+        log.debug( "get language of pdf from dublin core file" );
+       
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();     
+        Document document = builder.parse( xmlFile );
+
+        NodeList dcoai = document.getElementsByTagName( "oai_dc:dc" );
+        // assert dcoai.getLength() == 1
+        NodeList dcLanguage = ( (Element) dcoai.item( 0 ) ).getElementsByTagName( "dc:language" );
+        // assert dcLanguage.getLength() == 1
+        NodeList language = ( (Element) dcLanguage.item( 0 ) ).getChildNodes();
+        // assert language.getLength() == 1
+        String lang = language.item(0).getNodeValue();
+
+        cargoContainer.add( DataStreamNames.DublinCoreData, format, submitter, lang, mimetype, xmlBytes );
+       
+        log.debug( "read and add pdf to cargoContainer" );
+
         FilenameFilter[] pdfFilter = { new PdfFileFilter() };
         String pdfFilePath = FileHandler.getFileList( path, pdfFilter, false ).remove(0);
         log.debug( String.format( "pdf filepath='%s'", pdfFilePath ) );
-        byte[] pdfData = StreamHandler.bytesFromInputStream( FileHandler.readFile( pdfFilePath ), 0 );
-        cargoContainer.add( DataStreamNames.OriginalData, format, submitter, lang, mimetype, pdfData );
+        File pdfFile = FileHandler.getFile( pdfFilePath );
+        FileInputStream pdfData = FileHandler.readFile( pdfFilePath );
         
-        // read and add xml to cargoContainer
-        FilenameFilter[] xmlFilter = { new XmlFileFilter() };
-        String xmlFilePath = FileHandler.getFileList( path, xmlFilter, false ).remove(0);
-        log.debug( String.format( "xml filepath='%s'", xmlFilePath ) );
-        byte[] xmlData = StreamHandler.bytesFromInputStream( FileHandler.readFile( xmlFilePath ), 0 ); 
-        cargoContainer.add( DataStreamNames.DublinCoreData, format, submitter, lang, mimetype, xmlData );
+        byte pdfBytes[] = new byte[(int)pdfFile.length()];
+        pdfData.read( pdfBytes );
 
+        cargoContainer.add( DataStreamNames.OriginalData, format, submitter, lang, mimetype, pdfBytes );
+        
         return cargoContainer; 
     }
 }
