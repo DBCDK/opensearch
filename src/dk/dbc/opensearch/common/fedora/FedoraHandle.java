8bca04cd5f71a81e108188a1cc44d180cc878bdc
stm <stm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-02-23 11:46:06 +0000

    added autogeneration of soap stubs. FedoraHandle now uses config values for host, port, user, password.
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@3376 51cac136-f83d-432d-bb6f-125e83a60a2d

--- src/dk/dbc/opensearch/common/fedora/FedoraHandle.java
+++ src/dk/dbc/opensearch/common/fedora/FedoraHandle.java
@@ -3,11 +3,15 @@
  */
 package dk.dbc.opensearch.common.fedora;
 
+import dk.dbc.opensearch.common.helpers.FedoraConfig;
+
 import java.net.URL;
 
 import javax.xml.rpc.ServiceException;
 
 import org.apache.commons.configuration.XMLConfiguration;
+import org.apache.axis.client.Stub;
+import org.apache.axis.client.Call;
 
 import info.fedora.www.definitions._1._0.api.FedoraAPIA;
 import info.fedora.www.definitions._1._0.api.FedoraAPIAServiceLocator;
@@ -37,7 +41,7 @@ public abstract class FedoraHandle {
         /**
          * If needed by the fedora base, we can construct/get credentials here.
                    
-		log.debug( "Obtain config parameters for the fedora user");
+         log.debug( "Obtain config parameters for the fedora user");
         URL cfgURL = getClass().getResource("/config.xml");
         XMLConfiguration config = null;
         config = new XMLConfiguration( cfgURL );
@@ -47,16 +51,36 @@ public abstract class FedoraHandle {
         user       = config.getString( "fedora.user" );
         
         passphrase = config.getString( "fedora.passphrase" );
-        fedoraUrl  = "http://" + host + ":" + port + "/fedora";
+
         
-        log.debug( String.format( "Connecting to fedora server at:\n%s\n using user: %s, pass: %s ", fedoraUrl, user, passphrase ) );
         */
-        
-		m_locator = new FedoraAPIMServiceLocator();
-		// locator.setMaintainSession( false );
-		fem = m_locator.getFedoraAPIMServiceHTTPPort();
-		
-		a_locator = new FedoraAPIAServiceLocator();
-		fea = a_locator.getFedoraAPIAPortSOAPHTTP();
+
+            String fedora_base_url;
+
+            String host = FedoraConfig.getFedoraHost();
+            String port = FedoraConfig.getFedoraPort();
+            String user = FedoraConfig.getFedoraUser();
+            String pass = FedoraConfig.getFedoraPassPhrase();
+
+            fedora_base_url  = String.format( "http://%s:%s/fedora/services/management", host, port );
+
+            m_locator = new FedoraAPIMServiceLocator();
+
+            // log.debug( String.format( "Connecting to %s", fedora_base_url ) );
+
+            m_locator.setFedoraAPIMServiceHTTPPortEndpointAddress( fedora_base_url );
+
+            // locator.setMaintainSession( false );
+            fem = m_locator.getFedoraAPIMServiceHTTPPort();
+
+            // to use Basic HTTP Authentication:
+            ( ( Stub ) fem )._setProperty( Call.USERNAME_PROPERTY, user );
+            ( ( Stub ) fem )._setProperty( Call.PASSWORD_PROPERTY, pass );
+
+
+
+            a_locator = new FedoraAPIAServiceLocator();
+            fea = a_locator.getFedoraAPIAServiceHTTPPort();
+                          //getFedoraAPIAPortSOAPHTTP();
 	}
 }
