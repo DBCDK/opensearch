9ba68cec6dbbbc657401327ad8a3acdc6ff167ad
lvh <lvh@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-03-02 16:18:37 +0000

    comparator for comparing two pairs whos first element is a String
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@3546 51cac136-f83d-432d-bb6f-125e83a60a2d

---
+++ src/dk/dbc/opensearch/common/helpers/FirstStringComparator.java
@@ -0,0 +1,18 @@
+package dk.dbc.opensearch.common.helpers;
+
+import dk.dbc.opensearch.common.types.Pair;
+
+import java.util.Comparator;
+
+/**
+ * Helper class for the FedoraTolls class. Used to sorting arraylists
+ * of Pair<String, Object>. Sorting on the first element, the String in
+ * the Pair
+ */
+public class FirstStringComparator implements Comparator
+{
+    public int compare( Object x, Object y )
+    {
+        return ((Pair< String, Object >) x).getFirst().compareTo( ( (Pair<String, Object>)y).getFirst() );
+    }
+}
