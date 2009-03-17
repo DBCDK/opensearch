b844cdadb9f8b7339c01be90825b671977657ba4
lvh <lvh@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-03-17 11:34:16 +0000

    Changed the test back to how it was before. I'll close the bug on it.
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@3707 51cac136-f83d-432d-bb6f-125e83a60a2d

--- src/dk/dbc/opensearch/common/types/tests/CPMAliasTest.java
+++ src/dk/dbc/opensearch/common/types/tests/CPMAliasTest.java
@@ -3,31 +3,46 @@ package dk.dbc.opensearch.common.types.tests;
 
 import java.io.IOException;
 
-import javax.xml.parsers.ParserConfigurationException;
-
 import dk.dbc.opensearch.common.types.CPMAlias;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import org.junit.Test;
+import org.junit.*;
 import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
 
 
 public class CPMAliasTest 
 {
+    /**
+      * This test is not a strict unittest because we are dependant on a file on the disc.
+      */
+
+    CPMAlias cpmAlias;
+@Before
+    public void setUp() throws Exception
+    {
+       	cpmAlias = new CPMAlias();
+    }
+
+@After
+    public void tearDown() throws Exception
+    {
+        cpmAlias = null;
+    }
+
 	@Test
 	public void cpmIsValidAliasTest() throws ParserConfigurationException, SAXException, IOException
 	{
-		CPMAlias cpmAlias = new CPMAlias();
-		boolean valid = cpmAlias.isValidAlias( "article" );
-		assertTrue( valid );
-	}
+            boolean valid = cpmAlias.isValidAlias( "article" );
+            assertTrue( valid );
+        }
 	
 	
 	@Test
 	public void cpmIsValidAliasFailTest() throws ParserConfigurationException, SAXException, IOException
 	{
-		CPMAlias cpmAlias = new CPMAlias();
 		boolean inValid = cpmAlias.isValidAlias( "fejlmester" );
 		assertFalse( inValid );
 	}
