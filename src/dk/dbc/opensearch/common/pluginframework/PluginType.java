9578696c8c424b049663380b7a3ccbf464832d09
stm <stm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-04-14 08:22:39 +0000

    Added license header to DBC java files, solving bug #8697
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@3846 51cac136-f83d-432d-bb6f-125e83a60a2d

--- src/dk/dbc/opensearch/common/pluginframework/PluginType.java
+++ src/dk/dbc/opensearch/common/pluginframework/PluginType.java
@@ -1,6 +1,26 @@
 package dk.dbc.opensearch.common.pluginframework;
 
 
+/*
+*GNU, General Public License Version 3. If any software components linked 
+*together in this library have legal conflicts with distribution under GNU 3 it 
+*will apply to the original license type.
+*
+*Software distributed under the License is distributed on an "AS IS" basis,
+*WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+*for the specific language governing rights and limitations under the
+*License.
+*
+*Around this software library an Open Source Community is established. Please 
+*leave back code based upon our software back to this community in accordance to 
+*the concept behind GNU. 
+*
+*You should have received a copy of the GNU Lesser General Public
+*License along with this library; if not, write to the Free Software
+*Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+***** END LICENSE BLOCK ***** */
+
+
 public enum PluginType 
 {
 	HARVEST,
