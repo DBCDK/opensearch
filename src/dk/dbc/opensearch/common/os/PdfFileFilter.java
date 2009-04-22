899c25e272f08094d537aa08e89589c1811d7759
stm <stm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-04-22 13:38:55 +0000

    fixed misplaced comment sign.
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@3889 51cac136-f83d-432d-bb6f-125e83a60a2d

--- src/dk/dbc/opensearch/common/os/PdfFileFilter.java
+++ src/dk/dbc/opensearch/common/os/PdfFileFilter.java
@@ -2,6 +2,7 @@
  * \file PdfFileFilter.java
  * \brief The PdfFileFilter class
  * \package os;
+ */
 
 /*
    
@@ -22,7 +23,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with opensearch.  If not, see <http://www.gnu.org/licenses/>.
 */
- */
+
 
 package dk.dbc.opensearch.common.os;
 
