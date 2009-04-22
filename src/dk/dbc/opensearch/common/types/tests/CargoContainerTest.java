4f5a99a843761bb00299a835c7f6b5820ce5b8cf
stm <stm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-04-22 13:34:56 +0000

    license text inserted, license agreement enclosed with source. This closes bug #8697
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@3888 51cac136-f83d-432d-bb6f-125e83a60a2d

--- src/dk/dbc/opensearch/common/types/tests/CargoContainerTest.java
+++ src/dk/dbc/opensearch/common/types/tests/CargoContainerTest.java
@@ -1,5 +1,25 @@
 
 package dk.dbc.opensearch.common.types.tests;
+
+/*
+   
+This file is part of opensearch.
+Copyright © 2009, Dansk Bibliotekscenter a/s, 
+Tempovej 7-11, DK-2750 Ballerup, Denmark. CVR: 15149043
+
+opensearch is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+opensearch is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with opensearch.  If not, see <http://www.gnu.org/licenses/>.
+*/
 /** \brief UnitTest for CargoContainerT **/
 
 import dk.dbc.opensearch.common.types.CargoContainer;
@@ -41,7 +61,7 @@ public class CargoContainerTest
 
 
     @Before
-        public void SetUp() throws UnsupportedEncodingException
+    public void SetUp() throws UnsupportedEncodingException
     {
         dsn = DataStreamType.getDataStreamNameFrom( "originalData" );
         format = "forfatterweb";
@@ -61,7 +81,7 @@ public class CargoContainerTest
      * @throws IOException
      */
     @Test
-        public void testAdd() throws IOException
+    public void testAdd() throws IOException
     {
         CargoContainer cargo = new CargoContainer();
         assertEquals( 0, (long)cargo.getData().size() );
@@ -75,7 +95,8 @@ public class CargoContainerTest
      * @throws IOException
      *
      */
-    @Test public void testStreamSizeInContainer() throws IOException
+    @Test 
+    public void testStreamSizeInContainer() throws IOException
     {
         cargo.add( dsn, format, submitter, language, mimetype, data );
 
@@ -91,7 +112,7 @@ public class CargoContainerTest
 
 
     @Test(expected = NullPointerException.class)
-        public void testStreamCannotBeEmpty() throws IOException
+    public void testStreamCannotBeEmpty() throws IOException
     {
 
         byte[] is = new byte[0];
@@ -102,11 +123,14 @@ public class CargoContainerTest
         byte[] list = co.getBytes();
 
         if( list.length == 0)
+        {
             throw new NullPointerException();
+        }
     }
 
 
-    @Test public void testGetByteArrayPreservesUTF8() throws IOException, UnsupportedEncodingException
+    @Test 
+    public void testGetByteArrayPreservesUTF8() throws IOException, UnsupportedEncodingException
     {
         cargo.add( dsn, format, submitter, language, mimetype, data );
 
@@ -114,14 +138,15 @@ public class CargoContainerTest
         byte[] listB = aList.get( 0 ).getBytes();
         byte[] sixBytes = new byte[6];
         for( int i = 0; i < listB.length; i++ )
+        {
             sixBytes[i] = listB[i];
-
+        }
         assertTrue( teststring.equals( new String( sixBytes, "UTF-8" ) ) );
     }
 
 
     @Test
-        public void testItemsCount() throws IOException
+    public void testItemsCount() throws IOException
     {
         CargoContainer cc = new CargoContainer();
 
@@ -177,6 +202,8 @@ public class CargoContainerTest
         cc.setIndexingAlias( ia );
         assertTrue( ia == cc.getIndexingAlias() );
     }
+
+
     @Test public void testFilePathSetAndGet()
     {
         CargoContainer cc = new CargoContainer();
