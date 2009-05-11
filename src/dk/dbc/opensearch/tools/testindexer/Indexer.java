1f0575ec3c241ecb53b130b86f42ed53545675b3
shm <shm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-05-11 13:15:22 +0000

    some progress in integrating the testIndexer, and removed build errors
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@3976 51cac136-f83d-432d-bb6f-125e83a60a2d

--- src/dk/dbc/opensearch/tools/testindexer/Indexer.java
+++ src/dk/dbc/opensearch/tools/testindexer/Indexer.java
@@ -6,13 +6,16 @@
 
 package dk.dbc.opensearch.tools.testindexer;
 
+import org.compass.core.Compass;
+import dk.dbc.opensearch.common.compass.CompassFactory;
 import dk.dbc.opensearch.common.pluginframework.JobMapCreator;
 import dk.dbc.opensearch.common.types.DatadockJob;
 import dk.dbc.opensearch.common.types.InputPair;
 import dk.dbc.opensearch.common.types.Pair;
 import dk.dbc.opensearch.components.datadock.DatadockThread;
+import dk.dbc.opensearch.components.pti.PTIThread;
 import dk.dbc.opensearch.tools.testindexer.Estimate;
-import dk.dbc.opensearch.tools.testindexer.FedoraHandle;
+import dk.dbc.opensearch.tools.testindexer.FedoraCommunication;
 import dk.dbc.opensearch.tools.testindexer.Processqueue;
 
 import java.io.IOException;
@@ -24,6 +27,7 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
+import org.compass.core.CompassSession;
 
 import javax.xml.rpc.ServiceException;
 
@@ -39,49 +43,84 @@ import javax.xml.rpc.ServiceException;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.ExecutionException;
 import java.lang.InterruptedException;
+import dk.dbc.opensearch.common.fedora.IFedoraCommunication;
+import org.apache.log4j.Logger;
 
 public class Indexer{
-    private FedoraHandle fedorahandle;
-    //private Executor pool;
+
+    Logger log = Logger.getLogger( Indexer.class );
+
     private ExecutorService pool;
-    public static HashMap< InputPair< String, String >, ArrayList< String > > jobMap;
+    private static HashMap< InputPair< String, String >, ArrayList< String > > jobMap;
 
     public Indexer()throws ServiceException, MalformedURLException, IOException, ConfigurationException
     {
-        //        fedorahandle = new FedoraHandle();
-        //pool = Executors.newFixedThreadPool(1);
+
+        pool = Executors.newFixedThreadPool(1);
     }
 
-    public void index( URI uri, String submitter, String format ) throws ParserConfigurationException, SAXException, IOException, ConfigurationException, ClassNotFoundException, PluginResolverException, ServiceException, InterruptedException
+    public void index( URI uri, String submitter, String format, Compass compass ) throws ParserConfigurationException, SAXException, IOException, ConfigurationException, ClassNotFoundException, PluginResolverException, ServiceException, InterruptedException
     {
-        // DatadockJob datadockJob = new DatadockJob( uri, submitter, format);
-        // IEstimate e = new Estimate();
-        // IProcessqueue p = new Processqueue();
-        // jobMap = JobMapCreator.getMap( this.getClass() );
-        // DatadockThread ddt = new DatadockThread( datadockJob, e, p, jobMap );
-        // FutureTask ft = new FutureTask( ddt );
-        // pool.submit( ft );
-        // while(! ft.isDone() ){}
 
-        // Float f = -1f;
+        // Firstly, the data pointed to by uri, is worked on by a datadockThread
 
-        // try{
-        //         f = (Float)ft.get();
-        //     }
-        // catch( ExecutionException ee ){
-
-        //         // getting exception from thread
-        //         Throwable cause = ee.getCause();
-
-        //         System.err.println( String.format( "Exception Caught: '%s'\n'%s'", cause.getClass() , cause.getMessage() ) );
-        //         StackTraceElement[] trace = cause.getStackTrace();
-        //         for( int i = 0; i < trace.length; i++ )
-        //             {
-        //                 System.err.println( trace[i].toString() );
-        //             }
-        // }
+        DatadockJob datadockJob = new DatadockJob( uri, submitter, format);
+        IEstimate e = new Estimate();
+        IProcessqueue p = new Processqueue();
+        IFedoraCommunication c = new FedoraCommunication();
+        jobMap = JobMapCreator.getMap( this.getClass() );
         
-        
+        DatadockThread ddt = new DatadockThread( datadockJob, e, p, jobMap, c );
+        FutureTask ft = new FutureTask( ddt );
+        pool.submit( ft );
+        while(! ft.isDone() ){}
 
+        Float f = -1f;
+
+        try{
+            f = (Float)ft.get();
+        }
+        catch( ExecutionException ee ){
+
+            // getting exception from thread
+            Throwable cause = ee.getCause();
+            
+            System.err.println( String.format( "Exception Caught: '%s'\n'%s'", cause.getClass() , cause.getMessage() ) );
+            StackTraceElement[] trace = cause.getStackTrace();
+            for( int i = 0; i < trace.length; i++ )
+            {
+                System.err.println( trace[i].toString() );
+            }
+        }
+        
+        // After, the data is gone through the datadock, it is indexed through the pti thread
+
+        log.debug( "Setting up the Compass object" );
+        CompassSession session = compass.openSession();
+
+        PTIThread PTIt = new PTIThread( "mockPID", session, e, jobMap);
+        FutureTask ptiFuture = new FutureTask( PTIt );
+        pool.submit( ptiFuture );
+        while(! ptiFuture.isDone() ){}
+
+        Long l = 0l;
+
+        try{
+            l = (Long) ptiFuture.get();
+        }
+        catch( ExecutionException ee ){
+
+            // getting exception from thread
+            Throwable cause = ee.getCause();
+            
+            System.err.println( String.format( "Exception Caught: '%s'\n'%s'", cause.getClass() , cause.getMessage() ) );
+            StackTraceElement[] trace = cause.getStackTrace();
+            for( int i = 0; i < trace.length; i++ )
+            {
+                System.err.println( trace[i].toString() );
+            }
+        }
+
+        log.debug( String.format( "Indexed file: %s", uri ) );
     }
 }
--- src/dk/dbc/opensearch/tools/testindexer/IndexerMain.java
+++ src/dk/dbc/opensearch/tools/testindexer/IndexerMain.java
@@ -6,5 +6,18 @@
 
 package dk.dbc.opensearch.tools.testindexer;
 
+import dk.dbc.opensearch.common.compass.CompassFactory;
+import org.compass.core.Compass;
+import org.apache.commons.configuration.ConfigurationException;
+
+public class IndexerMain{
 
 
+static public void main(String[] args) throws ConfigurationException
+    {
+        CompassFactory compassFactory = new CompassFactory();
+        Compass compass = compassFactory.getCompass();
+            
+        System.out.println( "HEJ.... jeg er ikke implementeret endnu, nænej");    
+    }
+}
