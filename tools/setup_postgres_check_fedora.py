18f954a1bca0a53e8cc46b0a97f4d5ea6ff1d7ba
stm <stm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-06-09 12:06:58 +0000

    fixing issue mentioned in http://bugs.dbc.dk/show_bug.cgi?id=8760#c28
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@4180 51cac136-f83d-432d-bb6f-125e83a60a2d

--- tools/setup_postgres_check_fedora.py
+++ tools/setup_postgres_check_fedora.py
@@ -33,19 +33,36 @@ except ImportError:
     except ImportError:
         sys.exit( "could not import elementtree library. Is it installed?" )
 
+def main( harvest_folder ):
+    
+    build_config.main( [ '', '../' ] )
 
-build_config.main( [ '', '../' ] )
+    config = ET.parse( '../config/config.xml' )
 
-config = ET.parse( '../config/config.xml' )
+    dest = config.findall( '//toharvest' )[0].text
 
-dest = config.findall( '//toharvest' )[0].text
+    if os.path.exists( dest ):
+        shutil.rmtree( dest )
 
-if os.path.exists( dest ):
-    shutil.rmtree( dest )
+    shutil.copytree( harvest_folder, dest )
 
-shutil.copytree( '/data1/harvest-kanon', dest )
+    postgres_setup.main()
+    fedora_conn.test_fedora_conn( 'localhost', '8080' )
 
-postgres_setup.main()
-fedora_conn.test_fedora_conn( 'localhost', '8080' )
+if __name__ == '__main__':
 
+    from optparse import OptionParser
+    
+    parser = OptionParser( usage="%prog [options]" )
+    
+    parser.add_option( "-f", dest="harvest_folder", 
+                       action="store", help="The full path to the folder containing the data to be harvested" )
+    
+    (options, args) = parser.parse_args()
 
+    if not options.harvest_folder:
+        harvest_folder = '/data1/harvest-kanon'
+    else:
+        harvest_folder = options.harvest_folder
+        
+    main( harvest_folder )
