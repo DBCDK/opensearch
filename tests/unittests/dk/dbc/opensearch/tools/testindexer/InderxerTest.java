23116b2d37d71daa097c8ef75a0e840f71c3d851
shm <shm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-06-09 10:10:40 +0000

    changed silly method name, and cleaned up a bit
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@4176 51cac136-f83d-432d-bb6f-125e83a60a2d

--- tests/unittests/dk/dbc/opensearch/tools/testindexer/InderxerTest.java
+++ tests/unittests/dk/dbc/opensearch/tools/testindexer/InderxerTest.java
@@ -1,6 +1,6 @@
 /**
 This file is part of opensearch.
-Copyright © 2009, Dansk Bibliotekscenter a/s, 
+Copyright © 2009, Dansk Bibliotekscenter a/s,
 Tempovej 7-11, DK-2750 Ballerup, Denmark. CVR: 15149043
 
 opensearch is free software: you can redistribute it and/or modify
@@ -20,45 +20,43 @@ along with opensearch.  If not, see <http://www.gnu.org/licenses/>.
 
 package dk.dbc.opensearch.tools.testindexer;
 
-/** \brief UnitTest for Inderxer */
-import dk.dbc.opensearch.common.types.DatadockJob;
-import static org.junit.Assert.*;
-import org.junit.*;
-import org.compass.core.CompassSession;
-import dk.dbc.opensearch.tools.testindexer.Indexer;
+/** \brief UnitTest for Indexer */
+
+
 import dk.dbc.opensearch.common.db.IProcessqueue;
-// import dk.dbc.opensearch.common.types.CargoContainer;
-// import dk.dbc.opensearch.common.types.CargoMimeType;
-// import dk.dbc.opensearch.common.types.CompletedTask;
-import dk.dbc.opensearch.common.types.DatadockJob;
-import dk.dbc.opensearch.common.statistics.IEstimate;
-// import dk.dbc.opensearch.components.datadock.DatadockThread;
-// import dk.dbc.opensearch.components.datadock.DatadockPool;
 import dk.dbc.opensearch.common.fedora.IFedoraCommunication;
-// import dk.dbc.opensearch.common.fedora.PIDManager;
-// import dk.dbc.opensearch.common.config.DatadockConfig;
 import dk.dbc.opensearch.common.pluginframework.PluginResolverException;
+import dk.dbc.opensearch.common.statistics.IEstimate;
+import dk.dbc.opensearch.common.types.DatadockJob;
+import dk.dbc.opensearch.tools.testindexer.Indexer;
+
+import java.io.IOException;
+import java.lang.ClassNotFoundException;
+import java.lang.InterruptedException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 import java.util.concurrent.ThreadPoolExecutor;
-import org.compass.core.Compass;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.rpc.ServiceException;
+
 import mockit.Mock;
 import mockit.MockClass;
 import mockit.Mockit;
-import static org.easymock.classextension.EasyMock.*;
-import java.util.concurrent.FutureTask;
+
 import org.apache.commons.configuration.ConfigurationException;
-import java.io.IOException;
-import javax.xml.rpc.ServiceException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import javax.xml.parsers.ParserConfigurationException;
+import org.compass.core.Compass;
+import org.compass.core.CompassSession;
+import static org.easymock.classextension.EasyMock.*;
+import org.junit.*;
+import static org.junit.Assert.*;
 import org.xml.sax.SAXException;
-import java.lang.ClassNotFoundException;
-import java.lang.InterruptedException;
-import java.util.concurrent.ExecutionException;
 
 
 /**
- * 
+ *
  */
 public class InderxerTest
 {
@@ -77,26 +75,25 @@ public class InderxerTest
     public static class MockIndexer
     {
 
-        @Mock(invocations = 1)
+        @Mock( invocations = 1 )
         public static FutureTask getDatadockTask( DatadockJob datadockjob, IEstimate estimate, IProcessqueue processqueue, IFedoraCommunication fedoraCommunication )
         {
             return mockFutureDatadock;
         }
-        
-        @Mock(invocations = 1)
+
+        @Mock( invocations = 1 )
         public static FutureTask getPTITask( String fedoraPID, CompassSession session, IEstimate estimate, IFedoraCommunication fedoraCommunication )
         {
             return mockFuturePTI;
         }
-        
+
     }
 
     /**
-     * 
+     *
      */
-    @Test public void testFFFtest() throws ConfigurationException, IOException, ServiceException, URISyntaxException, ParserConfigurationException, SAXException, ClassNotFoundException, PluginResolverException, InterruptedException, ExecutionException
+    @Test public void testIndexer() throws ClassNotFoundException, ConfigurationException, ExecutionException, InterruptedException, IOException, ParserConfigurationException, PluginResolverException, SAXException, ServiceException, URISyntaxException
     {
-        // why does setup not work ?
         mockEstimate = createMock( IEstimate.class );
         mockProcessqueue = createMock( IProcessqueue.class );
         mockFedoraCommunication = createMock( IFedoraCommunication.class );
@@ -115,15 +112,15 @@ public class InderxerTest
         long testlong = 48l;
 
         expect( mockCompass.openSession() ).andReturn( mockSession );
-        
+
         expect( mockThreadPoolExecutor.submit( mockFutureDatadock ) ).andReturn( mockFutureDatadock );
         expect( mockThreadPoolExecutor.submit( mockFuturePTI ) ).andReturn( mockFuturePTI );
-        
+
         expect( mockFutureDatadock.isDone() ).andReturn( false );
         expect( mockFutureDatadock.isDone() ).andReturn( true );
         expect( mockFutureDatadock.get() ).andReturn( testfloat );
 
-        
+
         expect( mockFuturePTI.isDone() ).andReturn( false );
         expect( mockFuturePTI.isDone() ).andReturn( true );
         expect( mockFuturePTI.get() ).andReturn( testlong );
@@ -138,7 +135,7 @@ public class InderxerTest
 
         verify( mockCompass );
         verify( mockFutureDatadock );
-                verify( mockFuturePTI );
+        verify( mockFuturePTI );
         verify( mockThreadPoolExecutor );
 
         // teardown
@@ -152,3 +149,4 @@ public class InderxerTest
         reset( mockFuturePTI );
     }
 }
+
