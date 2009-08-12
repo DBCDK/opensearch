19acefbfafdbcc7963bbbbc5665e3197b3bf96b2
shm <shm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-08-12 15:17:18 +0000

    Small script to view the rows in processqueue and related tables
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@4956 51cac136-f83d-432d-bb6f-125e83a60a2d

---
+++ admin/showall.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -*- mode: python -*-
+
+
+# This file is part of opensearch.
+# Copyright © 2009, Dansk Bibliotekscenter a/s, 
+# Tempovej 7-11, DK-2750 Ballerup, Denmark. CVR: 15149043
+#
+# opensearch is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# opensearch is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with opensearch.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+# 
+
+
+import psycopg2
+import os
+
+
+def showall():
+    """
+    this script print the rows stored in the processqueue and related tables,
+    and prints a small summary.
+    """
+
+    format = "%-15s %-18s %-2s" 
+
+    usern = os.environ.get( 'USER' )
+    
+    try:
+        conn = psycopg2.connect( "dbname='%s' user='%s' password='%s'"%( usern, usern, usern))
+    except:
+        print "I am unable to connect to the database."
+       
+    cur = conn.cursor()
+    
+    try:
+        cur.execute("""SELECT * from processqueue""")
+    except:
+        print "I can't SELECT from processqueue"
+        
+    rows = cur.fetchall()
+
+    print "processqueue"
+    print "----------------------------------------"
+    print format % ( "queueID", "fedorahandle", "processing" )
+    for r in rows :
+        print format % ( r[0],r[1],r[2]  )
+
+
+    try:
+        cur.execute("""SELECT * from statistics""")
+    except:
+        print "I can't SELECT from statistics"
+        
+    rows = cur.fetchall()
+
+    print "\n\nstatistics"
+    print "----------------------------------------"
+    print format % ( "proccesstime", "dataamount", "mimetype")
+    for r in rows :
+        print format % ( r[0],r[1], r[2]  )
+
+
+    try:
+        cur.execute("""SELECT * from notindexed""")
+    except:
+        print "I can't SELECT from notindexed"
+        
+    rows = cur.fetchall()
+
+    print "\n\nnotindexed"
+    print "----------------------------------------"
+    print format % ( "queueID", "fedorahandle", "")
+    for r in rows :
+        print format % ( r[0],r[1], ""  )
+
+
+    try:
+        cur.execute("""SELECT COUNT(*) from processqueue""")
+    except:
+        print "I can't SELECT COUNT(*)from processqueue"
+        
+    total_rows = cur.fetchall()[0][0]
+
+    try:
+        cur.execute("""SELECT COUNT(*) from processqueue  WHERE processing='Y'""")
+    except:
+        print "I can't SELECT COUNT(*)from processqueue"
+
+    try:
+        active_rows = cur.fetchall()[0][0]
+    except:
+        active_rows = 0
+            
+    print "\n----------------------------------------"
+    print "Summary:"
+    print " Total Number of jobs in processqueue: %s\n      ( %s marked ready to process, %s marked as processing )." % (total_rows, total_rows-active_rows,active_rows)
+
+    
+    try:
+        cur.execute("""SELECT COUNT(*) from notindexed""")
+    except:
+        print "I can't SELECT COUNT(*)from notindexed"
+        
+    total_rows = cur.fetchall()[0][0]
+
+    print " Total Number of jobs in notindexed: %s" % (total_rows)
+
+
+showall()
