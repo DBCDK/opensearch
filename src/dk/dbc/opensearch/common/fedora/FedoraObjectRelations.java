c4fa6bd0567da051e6a637cd5c72bd71f9df8703
stm <stm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-08-12 14:38:16 +0000

    fedora relation class skeletal implementation and fixes of the strange fedoraclient error (two needed jar files added)
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@4953 51cac136-f83d-432d-bb6f-125e83a60a2d

--- src/dk/dbc/opensearch/common/fedora/FedoraObjectRelations.java
+++ src/dk/dbc/opensearch/common/fedora/FedoraObjectRelations.java
@@ -5,6 +5,7 @@
 package dk.dbc.opensearch.common.fedora;
 
 import fedora.common.rdf.FedoraRelsExtNamespace;
+import fedora.common.rdf.RDFName;
 import fedora.common.rdf.RDFNamespace;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 import javax.xml.rpc.ServiceException;
 import org.apache.commons.configuration.ConfigurationException;
+import org.apache.log4j.Logger;
 import org.trippi.TupleIterator;
 
 
@@ -23,16 +25,34 @@ import org.trippi.TupleIterator;
 public class FedoraObjectRelations
 {
 
-    public enum RELATIONSHIP
+    private static Logger log = Logger.getLogger( FedoraObjectRelations.class );
+
+    /**
+     * Helper class providing a type-safe way of referring relations in the
+     * fedora repository
+     */
+    public enum Relationship
     {
 
-        IS_MEMBER_OF;
+        /**
+         * expresses the relation isMemberOfCollection
+         */
+        IS_MEMBER_OF_COLLECTION( "isMemberOfCollection" );
+
+        RDFName predicate;
+        private RDFNamespace relNS;
+
+        Relationship( String predicate )
+        {
+            relNS = new FedoraRelsExtNamespace();
+            this.predicate = new RDFName( relNS, predicate );
+        }
+
+
     }
-    private RDFNamespace relNS;
 
     public FedoraObjectRelations()
     {
-        relNS = new FedoraRelsExtNamespace();
     }
 
 
@@ -46,10 +66,35 @@ public class FedoraObjectRelations
      * @throws MalformedURLException
      * @throws IOException
      */
-    public TupleIterator getRelationships( String pid, RELATIONSHIP relation ) throws ConfigurationException, ServiceException, MalformedURLException, IOException
+    public TupleIterator getRelationships( String pid, Relationship relation ) throws ConfigurationException, ServiceException, MalformedURLException, IOException
     {
-//        String query = String.format( "select $s from <#ri> where $s <%s> <%s>", "fedora-rels-ext:isMemberOf", "work:1");
-        String query = String.format( "select $s from <#ri> where $s <%s> <%s>", relation, pid );
+        /**
+         * FedoraClient.getTuples might throw:
+         * java.io.IOException: Error getting tuple iterator: Error parsing
+         * 	at fedora.client.FedoraClient.getTuples(FedoraClient.java:{linenumber})
+         *
+         * which (to the naive coder) could indicate that there was a problem with either
+         * a) the query
+         * b) the returned HttpInputStream that Trippis TupleIterator parses, or
+         * c) the connection used by the HttpInputStream
+         *
+         * In reality, FedoraClient swallows the original exception and rethrows
+         * (part of) it as a IOException. The real cause can be guessed at from
+         * TrippiExceptions stack trace:
+         * org.trippi.TrippiException: Error parsing
+         *	at org.trippi.io.SparqlTupleIterator.<init>(SparqlTupleIterator.java:47)
+         *	at org.trippi.TupleIterator.fromStream(TupleIterator.java:152)
+         *	at fedora.client.FedoraClient.getTuples(FedoraClient.java:705)
+         *	... local call stack ...
+         *Caused by: org.xmlpull.v1.XmlPullParserException: caused by: org.xmlpull.v1.XmlPullParserException: resource not found: /META-INF/services/org.xmlpull.v1.XmlPullParserFactory make sure that parser implementing XmlPull API is available
+         *
+         * Which the scarred hacker will recognize as a form of type 'jar hell';
+         * fedora uses one version of the xmlpull library and we use another. So,
+         * if we update the fedora-client.jar, we'll most probably need to update
+         * the xmlpull jar and the  xpp3 jar
+         */
+        String query = String.format( "select $s from <#ri> where $s <%s> <%s>", relation.predicate.qName, pid );
+        log.debug( String.format( "using query %s", query ) );
         Map<String, String> qparams = new HashMap<String, String>( 3 );
         qparams.put( "lang", "itql" );
         qparams.put( "flush", "true" );
@@ -74,7 +119,7 @@ public class FedoraObjectRelations
      * @throws IOException
      * @throws NoSuchMethodException
      */
-    public List<TupleIterator> getRelationships( List<String> pids, RELATIONSHIP relation ) throws ConfigurationException, ServiceException, MalformedURLException, IOException, NoSuchMethodException
+    public List<TupleIterator> getRelationships( List<String> pids, Relationship relation ) throws ConfigurationException, ServiceException, MalformedURLException, IOException, NoSuchMethodException
     {
         throw new NoSuchMethodException( "not implemented" );
     }
@@ -88,12 +133,10 @@ public class FedoraObjectRelations
      * @param collectionpid
      * @return
      */
-    public boolean addPidToCollection( String pid, String collectionpid) throws NoSuchMethodException
+    public boolean addPidToCollection( String pid, String collectionpid ) throws NoSuchMethodException
     {
         throw new NoSuchMethodException( "not implemented" );
     }
 
-
- 
 
 }
