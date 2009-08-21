850744638fbb24789b9e528b2394bd63057069e8
lvh <lvh@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-08-21 08:36:44 +0000

    changes inteface
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@5159 51cac136-f83d-432d-bb6f-125e83a60a2d

--- src/dk/dbc/opensearch/components/harvest/IHarvest.java
+++ src/dk/dbc/opensearch/components/harvest/IHarvest.java
@@ -20,7 +20,7 @@ along with opensearch.  If not, see <http://www.gnu.org/licenses/>.
 
 package dk.dbc.opensearch.components.harvest;
 
-import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Interface that defines the operations of a data-harvester The
@@ -53,7 +53,7 @@ public interface IHarvest
      * @param maxAmount specifies the maximum amount of jobs to be written to the {@link IJobList}
      * @returns an {@link IjobList} containing information about jobs that the requestor can obtain.
      */
-    ArrayList<IJob> getJobs( int maxAmount );
+    List<IJob> getJobs( int maxAmount );
 
 
     /**
