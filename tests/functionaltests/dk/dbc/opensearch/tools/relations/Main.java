88d6ff8b312cce63d54a14e415a1c7f7ba434991
stm <stm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-08-26 12:22:57 +0000

    classes for querying the resource index of a fedora repository
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@5224 51cac136-f83d-432d-bb6f-125e83a60a2d

--- tests/functionaltests/dk/dbc/opensearch/tools/relations/Main.java
+++ tests/functionaltests/dk/dbc/opensearch/tools/relations/Main.java
@@ -1,3 +1,22 @@
+/*
+  This file is part of opensearch.
+  Copyright © 2009, Dansk Bibliotekscenter a/s,
+  Tempovej 7-11, DK-2750 Ballerup, Denmark. CVR: 15149043
+
+  opensearch is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  opensearch is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with opensearch.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package dk.dbc.opensearch.tools.relations;
 
 import java.io.IOException;
@@ -60,7 +79,9 @@ public class Main {
         usage += " [url]     The url to the fedora base, eg. http://sempu.dbc.dk:8080/fedora\n";
         usage += " [user]    The username with which to log in to fedora\n";
         usage += " [pass]    The password with which to authorize the user in fedora\n";
-        usage += " [pid]     The pid to search for relations to\n";
+        usage += " [query]   The itql query to pass to fedora\n\n";
+        usage += " eg.";
+        usage += " java -jar -Durl=http://sempu:8080/fedora -Duser=fedoraAdmin -Dpass=fedoraAdmin -Dquery=\"select \$s \$p \$o from <#ri> where \$s \$p \$o\" dist/OpenSearch_ITQL.jar";
         return usage;
     }
 }
