8cb0e4d29c25b2923c447c07276ddd2dd2e412ba
shm <shm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-08-28 11:57:15 +0000

    promoted logline with stored pid to info
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@5251 51cac136-f83d-432d-bb6f-125e83a60a2d

--- src/dk/dbc/opensearch/common/fedora/FedoraAdministration.java
+++ src/dk/dbc/opensearch/common/fedora/FedoraAdministration.java
@@ -205,7 +205,7 @@ public class FedoraAdministration implements IFedoraAdministration
         String pid = FedoraHandle.getInstance().getAPIM().ingest( foxml, Constants.FOXML1_1.toString(), logm );// "info:fedora/fedora-system:FOXML-1.1", logm );
 
         //log.trace( String.format( "Submitted data %s", new String( foxml ) ) );
-        log.trace( String.format( "Submitted data, returning pid %s", pid ) );
+        log.info( String.format( "Submitted data, returning pid %s", pid ) );
 
         return pid;
     }
