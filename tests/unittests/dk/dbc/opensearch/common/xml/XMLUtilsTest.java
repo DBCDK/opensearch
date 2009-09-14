51b399ec775cc58796daea69ff266039ad5037cb
stm <stm@51cac136-f83d-432d-bb6f-125e83a60a2d>
2009-09-14 09:06:13 +0000

    Fixed bug 9497: xmlutilstest uses deprecatede methods
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@6158 51cac136-f83d-432d-bb6f-125e83a60a2d

--- tests/unittests/dk/dbc/opensearch/common/xml/XMLUtilsTest.java
+++ tests/unittests/dk/dbc/opensearch/common/xml/XMLUtilsTest.java
@@ -24,19 +24,24 @@ package dk.dbc.opensearch.common.xml;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringBufferInputStream;
 import java.io.UnsupportedEncodingException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Namespace;
 import javax.xml.transform.Source;
 import javax.xml.transform.TransformerException;
-import javax.xml.transform.dom.DOMResult;
-import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
 import org.junit.*;
+import static org.junit.Assert.*;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
@@ -88,18 +93,45 @@ public class XMLUtilsTest
         DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
         Document jobDocument = documentBuilder.parse( new ByteArrayInputStream( expectedResult.getBytes( "UTF-8" ) ) );
         String result = XMLUtils.xmlToString( jobDocument );
+        assertEquals( result, expectedResult );
+    }
+    
+    
+    @Test
+    public void testTransform() throws IOException, ParserConfigurationException, SAXException, TransformerException
+    {
+        String xsltString = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?><xsl:stylesheet version=\"1.0\" xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\"><xsl:template match=\"/\"><html><body><h2>My CD Collection</h2><table border=\"1\"><tr bgcolor=\"#9acd32\"><th>Title</th><th>Artist</th></tr><xsl:for-each select=\"catalog/cd\"><tr><td><xsl:value-of select=\"title\"/></td><td><xsl:value-of select=\"artist\"/></td></tr></xsl:for-each></table></body></html></xsl:template></xsl:stylesheet>";
 
-        assert( result == expectedResult );
+        String expectedResult = "<html><body><h2>My CD Collection</h2><table border=\"1\"><tr bgcolor=\"#9acd32\"><th>Title</th><th>Artist</th></tr><tr><td>Empire Burlesque</td><td>Bob Dylan</td></tr></table></body></html>";
+        InputStream xsltStringStream = new ByteArrayInputStream( xsltString.getBytes() );
+        InputStream xmlStringStream = new ByteArrayInputStream( xmlString.getBytes() );
+
+        Source xsltSource = new StreamSource( xsltStringStream );
+        Source xmlSource = new StreamSource( xmlStringStream );
+        Document result = XMLUtils.transform( xmlSource, xsltSource ); 
+
+        assertEquals( XMLUtils.xmlToString( result ), expectedResult );
     }
     
     @Test
-        public void testTransform() throws IOException, ParserConfigurationException, SAXException, TransformerException
+    public void testGetNamespaces() throws XMLStreamException
     {
-        Source xsltSource = new StreamSource( new StringBufferInputStream( xsltString ) );
-        Source xmlSource = new StreamSource( new StringBufferInputStream( xmlString ) );
-        Document result = XMLUtils.transform( xmlSource, xsltSource ); 
+        String nsXml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><ting:container xmlns:ting=\"http://www.dbc.dk/ting\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns=\"http://www.bs.dk/standards/MarcXchange\"><ting:dc xmlns:nested=\"info:hej\"></ting:dc></ting:container>";
+        Map<String,String> expectedNsList = new HashMap<String, String>();
+        expectedNsList.put( "", "http://www.bs.dk/standards/MarcXchange" );
+        expectedNsList.put( "ting", "http://www.dbc.dk/ting" );
+        expectedNsList.put( "dc", "http://purl.org/dc/elements/1.1/" );
+        expectedNsList.put( "nested", "info:hej" );
 
-        assert( XMLUtils.xmlToString( result ) == expectedResult );        
+        InputStream nsXmlStream = new ByteArrayInputStream( nsXml.getBytes() );
+        List<Namespace> nsList = XMLUtils.getNamespaces( nsXmlStream );
+
+        assertTrue( expectedNsList.size() == nsList.size() );
+
+        for( Namespace ns : nsList )
+        {
+            // we'll not test for prefixes, as they're just rubble
+            assertTrue( expectedNsList.containsValue( ns.getNamespaceURI() ) );
+        }
     }
-    
 }
