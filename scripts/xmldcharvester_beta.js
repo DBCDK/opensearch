2160f31ea0c47c4ecaaab80ee79430679c95ec0b
rlo <rlo@51cac136-f83d-432d-bb6f-125e83a60a2d>
2010-08-02 11:50:08 +0000

    add dkabm
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@24856 51cac136-f83d-432d-bb6f-125e83a60a2d

--- scripts/xmldcharvester_beta.js
+++ scripts/xmldcharvester_beta.js
@@ -12,3 +12,29 @@ function buildxml_beta( xml )
     return dc;
 
 }
+
+function dcFromMarc( xml )
+{
+
+    Log.info( "RLO: Entering javascript dcFromMarc" );
+
+    var dc = DcCreator.createDcFromMarc ( xml ); 
+
+    Log.info( "RLO: Leaving javascript" );
+
+    return dc;
+
+}
+
+function dcFromDkabm( xml )
+{
+
+    Log.info( "RLO: Entering javascript dcFromDkabm" );
+
+    var dc = DcCreator.createDcFromDKabm ( xml ); 
+
+    Log.info( "RLO: Leaving javascript" );
+
+    return dc;
+
+}
