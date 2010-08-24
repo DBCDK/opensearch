d6906e18862f82556df9dcc1bb7f4a1d975f2e65
rlo <rlo@51cac136-f83d-432d-bb6f-125e83a60a2d>
2010-08-24 07:34:33 +0000

    that instead of DbcAddiRelations
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@25193 51cac136-f83d-432d-bb6f-125e83a60a2d

--- scripts/DbcAddiRelations.use.js
+++ scripts/DbcAddiRelations.use.js
@@ -8,7 +8,7 @@ const DbcAddiRelations = function() {
     /**
     * Will also set the inverse relation http://oss.dbc.dk/rdf/dbcaddi#hasThemeMember
     */
-    DbcAddiRelations.isPartOfTheme = function ( pid1, pid2 ){
+    that.isPartOfTheme = function ( pid1, pid2 ){
         Log.info( "Start isPartOfTheme" );
         scriptClass.createRelation( pid2, "http://oss.dbc.dk/rdf/dbcaddi#hasThemeMember", pid1);
         scriptClass.createRelation( pid1, "http://oss.dbc.dk/rdf/dbcaddi#isPartOfTheme", pid2 );
@@ -16,35 +16,35 @@ const DbcAddiRelations = function() {
     };
 
 
-    DbcAddiRelations.hasRingtone = function ( pid1, pid2 ){
+    that.hasRingtone = function ( pid1, pid2 ){
         Log.info( "Start hasRingtone" );
         scriptClass.createRelation( pid1, "http://oss.dbc.dk/rdf/dbcaddi#hasRingtone", pid2 );
         Log.info( "End hasRingtone" );
     };
 
 
-    DbcAddiRelations.hasUserCreatedContent = function ( pid1, pid2 ){
+    that.hasUserCreatedContent = function ( pid1, pid2 ){
         Log.info( "Start hasUserCreatedContent" );
         scriptClass.createRelation( pid1, "http://oss.dbc.dk/rdf/dbcaddi#hasUserCreatedContent", pid2 );
         Log.info( "End hasUserCreatedContent" );
     };
 
 
-    DbcAddiRelations.hasFulltext = function ( pid1, pid2 ){
+    that.hasFulltext = function ( pid1, pid2 ){
         Log.info( "Start hasFulltext" );
         scriptClass.createRelation( pid1, "http://oss.dbc.dk/rdf/dbcaddi#hasFulltext", pid2 );
         Log.info( "End hasFulltext" );
     };
 
 
-    DbcAddiRelations.hasImageRepresentation = function ( pid1, pid2 ){
+    that.hasImageRepresentation = function ( pid1, pid2 ){
         Log.info( "Start hasImageRepresentation" );
         scriptClass.createRelation( pid1, "http://oss.dbc.dk/rdf/dbcaddi#hasImageRepresentation", pid2 );
         Log.info( "End hasImageRepresentation" );
     };
 
 
-    DbcAddiRelations.hasDemo = function ( pid1, pid2 ){
+    that.hasDemo = function ( pid1, pid2 ){
         Log.info( "Start hasDemo" );
         scriptClass.createRelation( pid1, "http://oss.dbc.dk/rdf/dbcaddi#hasDemo", pid2 );
         Log.info( "End hasDemo" );
@@ -53,7 +53,7 @@ const DbcAddiRelations = function() {
     /**
     * Will also set the inverse relation http://oss.dbc.dk/rdf/dbcbibaddi#hasReview
     */
-    DbcAddiRelations.isReviewOf = function ( pid1, pid2 ){
+    that.isReviewOf = function ( pid1, pid2 ){
         Log.info( "Start isReviewOf" );
         scriptClass.createRelation( pid2, "http://oss.dbc.dk/rdf/dbcbibaddi#hasReview", pid1);
         scriptClass.createRelation( pid1, "http://oss.dbc.dk/rdf/dbcaddi#isReviewOf", pid2 );
@@ -61,14 +61,14 @@ const DbcAddiRelations = function() {
     };
 
 
-    DbcAddiRelations.hasTutorial = function ( pid1, pid2 ){
+    that.hasTutorial = function ( pid1, pid2 ){
         Log.info( "Start hasTutorial" );
         scriptClass.createRelation( pid1, "http://oss.dbc.dk/rdf/dbcaddi#hasTutorial", pid2 );
         Log.info( "End hasTutorial" );
     };
 
 
-    DbcAddiRelations.hasTrailer = function ( pid1, pid2 ){
+    that.hasTrailer = function ( pid1, pid2 ){
         Log.info( "Start hasTrailer" );
         scriptClass.createRelation( pid1, "http://oss.dbc.dk/rdf/dbcaddi#hasTrailer", pid2 );
         Log.info( "End hasTrailer" );
@@ -77,7 +77,7 @@ const DbcAddiRelations = function() {
     /**
     * Will also set the inverse relation http://oss.dbc.dk/rdf/dbcaddi#hasCover
     */
-    DbcAddiRelations.isCoverOf = function ( pid1, pid2 ){
+    that.isCoverOf = function ( pid1, pid2 ){
         Log.info( "Start isCoverOf" );
         scriptClass.createRelation( pid2, "http://oss.dbc.dk/rdf/dbcaddi#hasCover", pid1);
         scriptClass.createRelation( pid1, "http://oss.dbc.dk/rdf/dbcaddi#isCoverOf", pid2 );
@@ -87,7 +87,7 @@ const DbcAddiRelations = function() {
     /**
     * Will also set the inverse relation http://oss.dbc.dk/rdf/dbcaddi#isSubjectDescriptionOf
     */
-    DbcAddiRelations.hasSubjectDescription = function ( pid1, pid2 ){
+    that.hasSubjectDescription = function ( pid1, pid2 ){
         Log.info( "Start hasSubjectDescription" );
         scriptClass.createRelation( pid2, "http://oss.dbc.dk/rdf/dbcaddi#isSubjectDescriptionOf", pid1);
         scriptClass.createRelation( pid1, "http://oss.dbc.dk/rdf/dbcaddi#hasSubjectDescription", pid2 );
@@ -95,7 +95,7 @@ const DbcAddiRelations = function() {
     };
 
 
-    DbcAddiRelations.hasSoundClip = function ( pid1, pid2 ){
+    that.hasSoundClip = function ( pid1, pid2 ){
         Log.info( "Start hasSoundClip" );
         scriptClass.createRelation( pid1, "http://oss.dbc.dk/rdf/dbcaddi#hasSoundClip", pid2 );
         Log.info( "End hasSoundClip" );
@@ -104,7 +104,7 @@ const DbcAddiRelations = function() {
     /**
     * Will also set the inverse relation http://oss.dbc.dk/rdf/dbcbib#isReviewOf
     */
-    DbcAddiRelations.hasReview = function ( pid1, pid2 ){
+    that.hasReview = function ( pid1, pid2 ){
         Log.info( "Start hasReview" );
         scriptClass.createRelation( pid2, "http://oss.dbc.dk/rdf/dbcbib#isReviewOf", pid1);
         scriptClass.createRelation( pid1, "http://oss.dbc.dk/rdf/dbcaddi#hasReview", pid2 );
@@ -114,7 +114,7 @@ const DbcAddiRelations = function() {
     /**
     * Will also set the inverse relation http://oss.dbc.dk/rdf/dbcaddi#isAuthorDescriptionOf
     */
-    DbcAddiRelations.hasAuthorDescription = function ( pid1, pid2 ){
+    that.hasAuthorDescription = function ( pid1, pid2 ){
         Log.info( "Start hasAuthorDescription" );
         scriptClass.createRelation( pid2, "http://oss.dbc.dk/rdf/dbcaddi#isAuthorDescriptionOf", pid1);
         scriptClass.createRelation( pid1, "http://oss.dbc.dk/rdf/dbcaddi#hasAuthorDescription", pid2 );
