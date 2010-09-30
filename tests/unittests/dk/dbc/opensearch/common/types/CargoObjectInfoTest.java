d7fae860e152750c1357ca63a0649171ec3c4331
damkjaer <damkjaer@51cac136-f83d-432d-bb6f-125e83a60a2d>
2010-09-30 08:48:42 +0000

    Done some work on 11673: CargoObjectInfoTest skal opdateres Added and revised unittests to conform to javadoc for CargoObjectInfo.
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@25864 51cac136-f83d-432d-bb6f-125e83a60a2d

--- tests/unittests/dk/dbc/opensearch/common/types/CargoObjectInfoTest.java
+++ tests/unittests/dk/dbc/opensearch/common/types/CargoObjectInfoTest.java
@@ -28,10 +28,9 @@ import dk.dbc.opensearch.common.types.CargoObject;
 import dk.dbc.opensearch.common.types.CargoObjectInfo;
 import dk.dbc.opensearch.common.types.DataStreamType;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.Date;
 
 import static org.junit.Assert.*;
 import org.junit.*;
@@ -44,64 +43,120 @@ import org.junit.*;
 
 public class CargoObjectInfoTest
 {
-    CargoMimeType cmt;
-    CargoObjectInfo coi;
-    CargoContainer cc;
-    
-    String test_submitter = "DBC";
-    String test_format = "test_format";
-    String test_lang = "DA";
-    String teststring;
-    byte[] data;
-    long id;
 
+    // Default values to use in CargoObjectInfo:
+    DataStreamType test_datastreamtype = DataStreamType.OriginalData;
+    CargoMimeType test_mimetype = CargoMimeType.TEXT_XML;;
+    String test_language = new String("da");
+    String test_submitter = new String("DBC");
+    String test_format = new String("test_format");
+    long test_id = 0L;
 
-    @Before 
-    public void SetUp() throws UnsupportedEncodingException, IOException
+    // default CargoObjectInfo used in several tests:
+    CargoObjectInfo default_coi = null;
+
+    @Before
+    public void setUp()
     {
-        cmt =  CargoMimeType.TEXT_XML;
-        cc = new CargoContainer();
-        
-        teststring = "æøå";
-        data = teststring.getBytes( "UTF-8" );
-    	DataStreamType dataStreamName = DataStreamType.OriginalData;
-        id = cc.add( dataStreamName, test_format, test_submitter, test_lang, cmt.getMimeType(), data );
-        // cc.setIndexingAlias( "dockbook", dataStreamName);
+	default_coi = new CargoObjectInfo( test_datastreamtype, test_mimetype, test_language,
+					   test_submitter, test_format, test_id );
     }
     
-    
+    @Test ( expected = IllegalArgumentException.class )
+    public void testConstructorDataStreamTypeNotNull()
+    {
+	new CargoObjectInfo( null, test_mimetype, test_language,
+			     test_submitter, test_format, test_id );
+    }
+
+    @Test ( expected = IllegalArgumentException.class )
+    public void testConstructorMimeTypeNotNull()
+    {
+	new CargoObjectInfo( test_datastreamtype, null, test_language,
+			     test_submitter, test_format, test_id );
+    }
+
+    @Test ( expected = IllegalArgumentException.class )
+    public void testConstructorLanguageNotNull()
+    {
+	new CargoObjectInfo( test_datastreamtype, test_mimetype, null,
+			     test_submitter, test_format, test_id );
+    }
+
+    @Test ( expected = IllegalArgumentException.class )
+    public void testConstructorSubmitterNotNull()
+    {
+	new CargoObjectInfo( test_datastreamtype, test_mimetype, test_language,
+			     null, test_format, test_id );
+    }
+
+    @Test ( expected = IllegalArgumentException.class )
+    public void testConstructorFormatNotNull()
+    {
+	new CargoObjectInfo( test_datastreamtype, test_mimetype, test_language,
+			     test_submitter, null, test_id );
+    }
+
     @Test
     public void testGetDataStreamType()
     {
-    	for ( CargoObject co : cc.getCargoObjects() )
-    	{
-    		assertEquals( DataStreamType.OriginalData, co.getDataStreamType() );
-    	}
+	assertTrue( default_coi.getDataStreamType().equals( test_datastreamtype ) );
     }
-    
+
+    /**
+     * This one is a little bit tricky - we use a variable of
+     * CargoMimeType as parameter, but when we retrieve the variable
+     * it is converted to a String - through
+     * CargoMimeType.getMimeType(). This test is therefore more of a
+     * whitebox test than a blackbox test.
+     */    
     @Test 
-    public void testCorrectnessOfgetFormat() 
+    public void testGetMimeType() 
     {
-        for( CargoObject co : cc.getCargoObjects() )
-        	assertTrue( test_format.equals( co.getFormat() ) );
-    	       
+	assertTrue( default_coi.getMimeType().equals( test_mimetype.getMimeType() ) );
     }
-    
-    
+
     @Test 
-    public void testCorrectnessOfgetSubmitter() 
+    public void testGetLanguage() 
     {    	
-    	for( CargoObject co : cc.getCargoObjects() )
-    		assertTrue( test_submitter.equals( co.getSubmitter() ) );
+	assertTrue( default_coi.getLanguage().equals( test_language ) );
     }
-    
-    
+
     @Test 
-    public void testCorrectnessOfgetMimeType() 
-    {
-        for( CargoObject co : cc.getCargoObjects() )
-        	assertTrue( cmt.getMimeType().equals( co.getMimeType() ) );
-    	       
+    public void testGetSubmitter() 
+    {    	
+	assertTrue( default_coi.getSubmitter().equals( test_submitter ) );
     }
-    
+
+    @Test 
+    public void testGetFormat() 
+    {
+	assertTrue( default_coi.getFormat().equals( test_format ) );
+    }
+
+    @Test 
+    public void testGetId() 
+    {
+	assertTrue( default_coi.getId() == test_id );
+    }
+
+    /**
+     * We can not test the getTimestamp exactly, but the documentation
+     * says that the timestamp is set on the time of
+     * creation. Therefore we can get a timestamp immediately before
+     * creating a CargoObjectInfo and immediately after. The
+     * getTimestamp() should return a timestamp somewhere between
+     * those two timestamps.
+     */
+    @Test
+    public void testGetTimestamp()
+    {
+	Date timestampBefore = new Date();
+	CargoObjectInfo coi = new CargoObjectInfo( DataStreamType.OriginalData, CargoMimeType.TEXT_XML,
+						   "da", "Submitter", "Format", 0L);
+	Date timestampAfter = new Date();
+
+	assertTrue( coi.getTimestamp() >= timestampBefore.getTime() );
+	assertTrue( coi.getTimestamp() <= timestampAfter.getTime() );
+    }
 }
