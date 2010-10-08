6b81ca2f59dcba4892ebc7472dc22835d63945bb
damkjaer <damkjaer@51cac136-f83d-432d-bb6f-125e83a60a2d>
2010-10-08 10:57:05 +0000

    Removed codelines which were commented out.
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@26003 51cac136-f83d-432d-bb6f-125e83a60a2d

--- tests/unittests/dk/dbc/opensearch/common/db/ProcessqueueTest.java
+++ tests/unittests/dk/dbc/opensearch/common/db/ProcessqueueTest.java
@@ -21,12 +21,6 @@
 package dk.dbc.opensearch.common.db;
 
 
-
-//import com.mockrunner.jdbc.*;
-//import com.mockrunner.mock.jdbc.MockConnection;
-//import com.mockrunner.jdbc.StatementResultSetHandler;
-//import com.mockrunner.mock.jdbc.MockResultSet;
-
 import java.sql.SQLException;
 import java.util.NoSuchElementException;
 
@@ -46,9 +40,6 @@ import org.junit.Test;
 /** \brief Unittest for Processqueue */
 public class ProcessqueueTest// extends BasicJDBCTestCaseAdapter
 {
-//    MockConnection connection;
-//    StatementResultSetHandler statementHandler;
-//    MockResultSet result;
     @Mocked PostgresqlDBConnection connection;
     List<Pair<String, Integer>> resultVector;
     Processqueue processqueue;
@@ -61,52 +52,15 @@ public class ProcessqueueTest// extends BasicJDBCTestCaseAdapter
         resultVector = new ArrayList<Pair<String, Integer>>();
     }
 
-    /**
-     * setup mock resultset vector
-     */
-//    public void setupResultset(){
-//
-//        result = statementHandler.createResultSet();
-//        result.addColumn( "fedorahandle" );
-//        result.addColumn( "queueID" );
-//
-//        Object r[] =  { "handle1", 1 };
-//        result.addRow( r );
-//        resultVector.add( new Pair<String, Integer>( (String) r[0], (Integer) r[1] ) );
-//        r[0] = "handle2";
-//        r[1] = 2;
-//        result.addRow( r );
-//        resultVector.add( new Pair<String, Integer>( (String) r[0], (Integer) r[1] ) );
-//        r[0] = "handle3";
-//        r[1] = 3;
-//        result.addRow( r );
-//        resultVector.add( new Pair<String, Integer>( (String) r[0], (Integer) r[1] ) );
-//        r[0] = "handle4";
-//        r[1] = 4;
-//        result.addRow( r );
-//        resultVector.add( new Pair<String, Integer>( (String) r[0], (Integer) r[1] ) );
-//        r[0] = "handle5";
-//        r[1] = 5;
-//        result.addRow( r );
-//        resultVector.add( new Pair<String, Integer>( (String) r[0], (Integer) r[1] ) );
-//    }
-//
-
     @Ignore
     @Test
     public void testPush() throws ClassNotFoundException, SQLException
     {
-
         String mockFedorahandle = "mockhandle";
         String sql_query = (  String.format( "INSERT INTO processqueue( queueid, fedorahandle, processing ) "+
                                              "VALUES( nextval( 'processqueue_sequence' ) ,'%s','N' )", mockFedorahandle ) );
         
         processqueue.push( mockFedorahandle );
-
-//        verifySQLStatementExecuted( sql_query );
-//        verifyCommitted();
-//        verifyAllStatementsClosed();
-//        verifyConnectionClosed();
     }
 
     @Ignore
@@ -117,38 +71,13 @@ public class ProcessqueueTest// extends BasicJDBCTestCaseAdapter
 
         String sql_query = (  String.format( "INSERT INTO processqueue( queueid, fedorahandle, processing ) "+
                                              "VALUES( nextval( 'processqueue_sequence' ) ,'%s','N' )", mockFedorahandle ) );
-
-//        statementHandler.prepareThrowsSQLException( sql_query, new SQLException( "test exception" ));
-//
-//        try{
-//            processqueue.push( mockFedorahandle );
-//            fail("Should have thrown SQLException");
-//        }
-//        catch( SQLException sqlex ){ /* intentional */ }
-//
-//        verifyNotCommitted();
-//        verifyAllStatementsClosed();
-//        verifyConnectionClosed();
     }
 
     @Ignore
     @Test
     public void testPopAll() throws SQLException
     {
-
         String sql_query = "SELECT * from get_all_posts()";
-//        setupResultset();
-//        statementHandler.prepareGlobalResultSet( result );
-//
-//        Vector<Pair<String, Integer>> r = new Vector<Pair<String, Integer>>();
-//        r = processqueue.popAll();
-//
-//        assertEquals( r, resultVector );
-//
-//        verifySQLStatementExecuted( sql_query );
-//        verifyCommitted();
-//        verifyAllStatementsClosed();
-//        verifyConnectionClosed();
     }
 
 
@@ -156,314 +85,7 @@ public class ProcessqueueTest// extends BasicJDBCTestCaseAdapter
     @Test
     public void testPopAllExceptionThrown()
     {
-
         String sql_query = "SELECT * from get_all_posts()";
-        
-//        statementHandler.prepareThrowsSQLException( sql_query, new SQLException( "test exception" ));
-//        Vector<Pair<String, Integer>> r = new Vector<Pair<String, Integer>>();
-//
-//        try{
-//            r = processqueue.popAll();
-//            fail("Should have thrown SQLException");
-//        }
-//        catch( SQLException sqlex ){ /* intentional */ }
-//
-//        verifyNotCommitted();
-//        verifyAllStatementsClosed();
-//        verifyConnectionClosed();
     }
 
-    @Ignore
-    @Test
-    public void testPopLessThanMaxSize() throws SQLException
-    {
-//        setupResultset();
-//        int maxSize = 5;
-//        String sql_query = String.format( "SELECT * from get_posts( %s )", maxSize );
-//
-//        statementHandler.prepareGlobalResultSet( result );
-//
-//        Vector<Pair<String, Integer>> r = new Vector<Pair<String, Integer>>();
-//        r = processqueue.pop( maxSize );
-//
-//        assertEquals( r, resultVector );
-//
-//        verifySQLStatementExecuted( sql_query );
-//        verifyCommitted();
-//        verifyAllStatementsClosed();
-//        verifyConnectionClosed();
-    }
-    
-
-//    public void testPopMoreOrEquealToMaxSize() throws SQLException
-//    {
-//        setupResultset();
-//        int maxSize = 15;
-//        String sql_query = String.format( "SELECT * from get_posts( %s )", maxSize );
-//
-//        statementHandler.prepareGlobalResultSet( result );
-//
-//
-//        List<Pair<String, Integer>> r = new ArrayList<Pair<String, Integer>>();
-//        r = processqueue.pop( maxSize );
-//
-//        assertEquals( r, resultVector );
-//
-//        verifySQLStatementExecuted( sql_query );
-//        verifyCommitted();
-//        verifyAllStatementsClosed();
-//        verifyConnectionClosed();
-//    }
-//
-    
-//    public void testPopExceptionThrown()
-//    {
-//        int maxSize = 15;
-//        String sql_query = String.format( "SELECT * from get_posts( %s )", maxSize );
-//
-//        statementHandler.prepareThrowsSQLException( sql_query, new SQLException( "test exception" ));
-//        Vector<Pair<String, Integer>> r = new Vector<Pair<String, Integer>>();
-//
-//        try{
-//            r = processqueue.pop( maxSize );
-//            fail("Should have thrown SQLException");
-//        }
-//        catch( SQLException sqlex ){ /* intentional */ }
-//
-//        verifyNotCommitted();
-//        verifyAllStatementsClosed();
-//        verifyConnectionClosed();
-//    }
-
-
-//    public void testCommitWithValidQueueID() throws ConfigurationException, ClassNotFoundException, SQLException
-//    {
-//        statementHandler.prepareGlobalUpdateCount( 1 );
-//        int testQueueID = 10;
-//
-//        processqueue.commit( testQueueID );
-//        String sql_query = String.format( "DELETE FROM processqueue WHERE queueid = %s", testQueueID );
-//        verifySQLStatementExecuted( sql_query );
-//        verifyCommitted();
-//        verifyAllStatementsClosed();
-//        verifyConnectionClosed();
-//    }
-//
-//
-//    public void testCommitWithInvalidQueueID() throws ConfigurationException, ClassNotFoundException, SQLException
-//    {
-//        statementHandler.prepareGlobalUpdateCount( 0 );
-//        int testQueueID = 10;
-//
-//        try
-//        {
-//            processqueue.commit( testQueueID );
-//            fail("Should have gotten NoSuchElementException - returned zero rows removed from processqueue table");
-//        }
-//        catch(NoSuchElementException nse){ /* intentional */ }
-//
-//        String sql_query = String.format( "DELETE FROM processqueue WHERE queueid = %s", testQueueID );
-//        verifySQLStatementExecuted( sql_query );
-//
-//        verifyNotCommitted();
-//        verifyAllStatementsClosed();
-//        verifyConnectionClosed();
-//    }
-//
-//
-//    public void testRollbackWithValidQueueID() throws ConfigurationException, ClassNotFoundException, SQLException
-//    {
-//        statementHandler.prepareGlobalUpdateCount( 1 );
-//        int testQueueID = 10;
-//        processqueue.rollback( testQueueID );
-//
-//        String sql_query = String.format( "UPDATE processqueue SET processing = 'N' WHERE queueid = %s", testQueueID );
-//        verifySQLStatementExecuted( sql_query );
-//        verifyCommitted();
-//        verifyAllStatementsClosed();
-//        verifyConnectionClosed();
-//
-//    }
-//
-//
-//    public void testRollbackWithInvalidQueueID() throws ConfigurationException, ClassNotFoundException, SQLException
-//    {
-//        statementHandler.prepareGlobalUpdateCount( 0 );
-//        int testQueueID = 10;
-//
-//        try
-//        {
-//            processqueue.rollback( testQueueID );
-//            fail("Should have gotten NoSuchElementException - returned zero rows removed from processqueue table");
-//        }
-//        catch(NoSuchElementException nse){ /* intentional */ }
-//
-//        String sql_query = String.format( "UPDATE processqueue SET processing = 'N' WHERE queueid = %s", testQueueID );
-//        verifySQLStatementExecuted( sql_query );
-//        verifyNotCommitted();
-//        verifyAllStatementsClosed();
-//        verifyConnectionClosed();
-//    }
-//
-//
-//    public void testDeActivate()throws ClassNotFoundException, SQLException
-//    {
-//        statementHandler.prepareGlobalUpdateCount( 2 );
-//
-//        processqueue.deActivate();
-//
-//        String sql_query = "UPDATE processqueue SET processing = 'N' WHERE processing = 'Y'";
-//
-//        verifySQLStatementExecuted( sql_query );
-//        verifyCommitted();
-//        verifyAllStatementsClosed();
-//        verifyConnectionClosed();
-//    }
-//
-//    public void testDeActivateExceptionThrown()throws ClassNotFoundException
-//    {
-//        String sql_query = "UPDATE processqueue SET processing = 'N' WHERE processing = 'Y'";
-//
-//        statementHandler.prepareThrowsSQLException( sql_query, new SQLException( "test exception" ));
-//
-//        try{
-//            processqueue.deActivate();
-//            fail("Should have thrown SQLException");
-//        }
-//        catch( SQLException sqlex ){ /* intentional */ }
-//
-//        verifyNotCommitted();
-//        verifyAllStatementsClosed();
-//        verifyConnectionClosed();
-//    }
-//
-//
-//    public void testNotDocked() throws ClassNotFoundException, SQLException
-//    {
-//
-//        String mockPath = "mockpath";
-//
-//        String sql_query = (  String.format( "INSERT INTO notdocked( path ) "+
-//                                             "VALUES( %s )", mockPath ) );
-//
-//        processqueue.notDocked( mockPath );
-//
-//        verifySQLStatementExecuted( sql_query );
-//        verifyCommitted();
-//        verifyAllStatementsClosed();
-//        verifyConnectionClosed();
-//    }
-//
-//
-//    public void testNotDockedExceptionThrown() throws ClassNotFoundException
-//    {
-//        String mockPath = "mockpath";
-//
-//        String sql_query = (  String.format( "INSERT INTO notdocked( path ) "+
-//                                             "VALUES( %s )", mockPath ) );
-//
-//        statementHandler.prepareThrowsSQLException( sql_query, new SQLException( "test exception" ));
-//
-//        try{
-//            processqueue.notDocked( mockPath );
-//            fail("Should have thrown SQLException");
-//        }
-//        catch( SQLException sqlex ){ /* intentional */ }
-//
-//        verifyNotCommitted();
-//        verifyAllStatementsClosed();
-//        verifyConnectionClosed();
-//    }
-//
-//    public void testNotIndexed() throws ClassNotFoundException, SQLException
-//    {
-//        int queueID = 1;
-//        String sql_query1 = String.format( "SELECT * FROM processqueue WHERE queueID = %s", queueID );
-//
-//        result = statementHandler.createResultSet();
-//        result.addColumn( "fedorahandle" );
-//        result.addColumn( "queueID" );
-//
-//        Object r[] =  { "handle1", 1 };
-//        result.addRow( r );
-//
-//        statementHandler.prepareGlobalResultSet( result );
-//
-//        String sql_query2 = String.format( "INSERT INTO notindexed( queueid, fedorahandle ) VALUES( %s, '%s' )", r[1], r[0] );
-//
-//        String sql_query3 = String.format( "DELETE FROM processqueue WHERE queueID = %s", queueID );
-//
-//        statementHandler.prepareGlobalUpdateCount( 1 );
-//
-//        processqueue.notIndexed( queueID );
-//
-//        verifySQLStatementExecuted( sql_query1 );
-//        verifySQLStatementExecuted( sql_query2 );
-//        verifySQLStatementExecuted( sql_query3 );
-//        verifyCommitted();
-//        verifyAllResultSetsClosed();
-//        verifyConnectionClosed();
-//    }
-//
-//
-//    public void testNotIndexedThrowsExceptionQuery1() throws ClassNotFoundException, SQLException
-//    {
-//        int queueID = 1;
-//        String sql_query1 = String.format( "SELECT * FROM processqueue WHERE queueID = %s", queueID );
-//        Object r[] =  { "handle1", 1 };
-//
-//        statementHandler.prepareThrowsSQLException( sql_query1, new SQLException( "test exception" ));
-//
-//        String sql_query2 = String.format( "INSERT INTO notindexed( queueid, fedorahandle ) VALUES( %s, '%s' )", r[1], r[0] );
-//
-//        String sql_query3 = String.format( "DELETE FROM processqueue WHERE queueID = %s", queueID );
-//
-//        statementHandler.prepareGlobalUpdateCount( 1 );
-//
-//
-//        try{
-//            processqueue.notIndexed( queueID );
-//            fail("Should have thrown SQLException");
-//        }
-//        catch( SQLException sqlex ){ /* intentional */ }
-//
-//        verifySQLStatementNotExecuted( sql_query2 );
-//        verifySQLStatementNotExecuted( sql_query3 );
-//        verifyNotCommitted();
-//        verifyAllResultSetsClosed();
-//        verifyConnectionClosed();
-//    }
-//
-//    public void testNotIndexedThrowsExceptionQuery2() throws ClassNotFoundException, SQLException
-//    {
-//        int queueID = 1;
-//        String sql_query1 = String.format( "SELECT * FROM processqueue WHERE queueID = %s", queueID );
-//
-//        result = statementHandler.createResultSet();
-//        result.addColumn( "fedorahandle" );
-//        result.addColumn( "queueID" );
-//
-//        Object r[] =  { "handle1", 1 };
-//        result.addRow( r );
-//
-//        statementHandler.prepareGlobalResultSet( result );
-//
-//        String sql_query2 = String.format( "INSERT INTO notindexed( queueid, fedorahandle ) VALUES( %s, '%s' )", r[1], r[0] );
-//
-//        String sql_query3 = String.format( "DELETE FROM processqueue WHERE queueID = %s", queueID );
-//
-//        statementHandler.prepareThrowsSQLException( sql_query2, new SQLException( "test exception" ));
-//
-//        try{
-//            processqueue.notIndexed( queueID );
-//            fail("Should have thrown SQLException");
-//        }
-//        catch( SQLException sqlex ){ /* intentional */ }
-//
-//        verifySQLStatementExecuted( sql_query1 );
-//        verifySQLStatementNotExecuted( sql_query3 );
-//        verifyNotCommitted();
-//        verifyAllResultSetsClosed();
-//        verifyConnectionClosed();
-//    }
 }
