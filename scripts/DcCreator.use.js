a6481d6b287910d456fc296eb562a132d029cc55
rlo <rlo@51cac136-f83d-432d-bb6f-125e83a60a2d>
2011-01-31 14:03:30 +0000

    remember to retart datadock!
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@28133 51cac136-f83d-432d-bb6f-125e83a60a2d

--- scripts/DcCreator.use.js
+++ scripts/DcCreator.use.js
@@ -139,9 +139,6 @@ const DcCreator = function(){
       dcXml.oai_dc::dc = DcCreator.createElement("DELETED OBJECT", "type", dc);
     }
     
-    dcXml.oai_dc::dc += DcCreator.createElement( String(originalXml.dkabm::record.ac::activity.ac::action), "type", dc );
-
-    
     if (String(originalXml.dkabm::record.ac::activity.ac::action) === "delete-out-of-scope" ) {
       dcXml.oai_dc::dc = DcCreator.createElement("DELETED OBJECT", "type", dc);
     }
