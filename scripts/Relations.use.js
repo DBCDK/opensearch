b8be2068e9e0004da73f873f17307fc3386a387f
lsk <lsk@51cac136-f83d-432d-bb6f-125e83a60a2d>
2011-05-26 11:58:33 +0000

    Filmstriben OnlineAccess updated
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@30244 51cac136-f83d-432d-bb6f-125e83a60a2d

--- scripts/Relations.use.js
+++ scripts/Relations.use.js
@@ -733,9 +733,9 @@ var Relations = function() {
 
   };
   
-  that.hasFilmstribenOnlineAccess = function( xml, pid ) {
+  that.hasFilmstribenFSSOnlineAccess = function( xml, pid ) {
 
-    Log.info ("Start hasFilmstribenOnlineAccess" );
+    Log.info ("Start hasFilmstribenFSSOnlineAccess" );
 
     // Converting the xml-string to an XMLObject which e4x can handle:
     var manifestationXML = XmlUtil.fromString( xml );
@@ -746,10 +746,44 @@ var Relations = function() {
       DbcAddiRelations.hasOnlineAccess( pid, url );
     }
     
-    Log.info ("End hasFilmstribenOnlineAccess" );
+    Log.info ("End hasFilmstribenFSSOnlineAccess" );
 
   };
   
+  that.hasFilmstribenFSFOnlineAccess = function( xml, pid ) {
+
+    Log.info ("Start hasFilmstribenFSFOnlineAccess" );
+
+    // Converting the xml-string to an XMLObject which e4x can handle:
+    var manifestationXML = XmlUtil.fromString( xml );
+
+    if (String(manifestationXML.*.*.*.(@tag=='032').*.(@code=='x')).match(/FSF/)) {
+      var id = String(manifestationXML.*.*.*.(@tag=='856').*.(@code=='u')).replace(/http:\/\/www.filmstriben.dk\/\?showfilm=(.*)/, "$1");
+      var url = String("http://www.filmstriben.dk/fjernleje/filmdetails.aspx?id=" + id);
+      DbcAddiRelations.hasOnlineAccess( pid, url );
+    }
+    
+    Log.info ("End hasFilmstribenFSFOnlineAccess" );
+
+  };
+
+  that.hasFilmstribenFSBOnlineAccess = function( xml, pid ) {
+
+    Log.info ("Start hasFilmstribenFSBOnlineAccess" );
+
+    // Converting the xml-string to an XMLObject which e4x can handle:
+    var manifestationXML = XmlUtil.fromString( xml );
+
+    if (String(manifestationXML.*.*.*.(@tag=='032').*.(@code=='x')).match(/FSB/)) {
+      var id = String(manifestationXML.*.*.*.(@tag=='856').*.(@code=='u')).replace(/http:\/\/www.filmstriben.dk\/\?showfilm=(.*)/, "$1");
+      var url = String("http://www.filmstriben.dk/bibliotek/filmdetails.aspx?id=" + id);
+      DbcAddiRelations.hasOnlineAccess( pid, url );
+    }
+    
+    Log.info ("End hasFilmstribenFSBOnlineAccess" );
+
+  };
+
   that.hasSpecificOnlineAccess = function( xml, pid ) {
 
     Log.info ("Start hasSpecificOnlineAccess" );
