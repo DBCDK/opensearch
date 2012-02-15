c77eb7dd54c165ade5cd2b7e7c34bb6240ad7b4d
kwc <kwc@51cac136-f83d-432d-bb6f-125e83a60a2d>
2012-02-15 11:04:07 +0000

    working on Relations.use.js
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@38146 51cac136-f83d-432d-bb6f-125e83a60a2d

--- scripts/Relations.use.js
+++ scripts/Relations.use.js
@@ -152,7 +152,7 @@ var Relations = function() {
 						Log.info("kwc9 analysedTitle before: " + analysedTitle);
 					analysedTitle = Normalize.removeSpecialCharacters(analysedTitle); //normalizing because the field title in dc stream in which we search is normalized
 						Log.info("kwc10 analysedTitle after: " + analysedTitle);
-					analysedTitle = String(analysedTitle).replace(/¤/g, "");
+					analysedTitle = String(analysedTitle).replace(/\u00A4/g, "");
 					Log.info("kwc11 analysedTitle tryself: " + analysedTitle);
 					
 					analysedTitles.push (analysedTitle);
