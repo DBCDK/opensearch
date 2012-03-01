dbebb8c5a22957800a289274114e5ced5e32bf38
kwc <kwc@51cac136-f83d-432d-bb6f-125e83a60a2d>
2012-03-01 12:15:33 +0000

    workind on isAnalysisOf
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@38567 51cac136-f83d-432d-bb6f-125e83a60a2d

--- scripts/Relations.use.js
+++ scripts/Relations.use.js
@@ -180,18 +180,22 @@ that.isAnalysisOf = function ( xml, pid ) {
 	                                         
 	          Log.info("kwc4 query: " + query);
 	                                          
-	          var results = FedoraCQLSearch.search(query);
+						var results = FedoraCQLSearch.search(query);
 						
 						if (String(personNames[x]).match(/\(/)){
 							Log.info("kwc41 personName with born: " + personNames[x]);
 							var personNameNoBirth = String(personNames[x].split("\(",1));
-							Log.info("kwc42 type: " + typeof personNameNoBirth);
+							personNameNoBirth = personNameNoBirth.replace(/\s+$/, '');
+
+							
+						/*	Log.info("kwc42 type: " + typeof(personNameNoBirth));
 							personNameNoBirth = String(personNameNoBirth);
 							Log.info("kwc43 type: " + typeof personNameNoBirth);
 							personNameNoBirth = personNameNoBirth.trim();
-							Log.info("kwc44 type: " + typeof personNameNoBirth);
+							Log.info("kwc44 type: " + typeof personNameNoBirth); */
 							
 							Log.info("kwc45 personName with born taken away: " + personNameNoBirth);
+							
 							query = "creator \u003D " + personNameNoBirth + " AND " + "title \u003D " + analysedTitles[y];
 							var extraResults = FedoraCQLSearch.search(query);
 							results.push.apply(results, extraResults); //adds the values of extraResults to results
