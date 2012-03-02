c313756661943f5a022b5ec989edee24082b8a5c
kwc <kwc@51cac136-f83d-432d-bb6f-125e83a60a2d>
2012-03-02 12:41:46 +0000

    working on isAnalysisOf
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@38621 51cac136-f83d-432d-bb6f-125e83a60a2d

--- scripts/Relations.use.js
+++ scripts/Relations.use.js
@@ -192,7 +192,7 @@ that.isAnalysisOf = function ( xml, pid ) {
 							
 							query = "creator \u003D " + personNameNoBirth + " AND " + "title \u003D " + analysedTitles[y];
 							var extraResults = FedoraCQLSearch.search(query);
-	          	for (var xx = 0; xx < results.length; ++xx) {
+	          	for (var xx = 0; xx < extraResults.length; ++xx) {
 	          		var extraResult = extraResults[xx]; 
 								Log.info("kwc43 extraResult: " + extraResult);           
 		            if (!String(extraResult).match(/work:.*/)) {
