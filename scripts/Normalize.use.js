a919e151b21bb86a29325ee48b72c41f3716d17a
lsk <lsk@51cac136-f83d-432d-bb6f-125e83a60a2d>
2012-05-29 12:12:04 +0000

    testing Normalize function in Relations.use.js
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@40901 51cac136-f83d-432d-bb6f-125e83a60a2d

--- scripts/Normalize.use.js
+++ scripts/Normalize.use.js
@@ -1,7 +1,7 @@
 EXPORTED_SYMBOLS = ['Normalize'];
 
-use ( "XmlUtil" );
-use ( "XmlNamespaces" );
+use ( "XmlUtil.use.js" );
+use ( "XmlNamespaces.use.js" );
 
 var Normalize = function(){
 
@@ -9,19 +9,21 @@ var Normalize = function(){
 
   that.removeSpecialCharacters = function ( string ) {
 
-    var newString = string.replace(/\?|!|\*|'|\u00A4/g, "");
+    //var newString = string.replace(/\?|!|\*|'|\u00A4/g, "");
+		var newString = string.replace(/\?|!|\*|'|\(|\)|¤|,/g, "");
+		newString = newString.replace(/  /g, " ");
 		
 		return newString
 
   };
 
   that.removeSpecialCharacters.__doc__ = <doc type="method">
-    <brief>Method that extracts data from Open Search Object XML to create Dublin Core data</brief>
-    <syntax>DcCreator.removeSpecialCharacters( string )</syntax>
+    <brief>Method that removes certain special characters from a string</brief>
+    <syntax>Normalize.removeSpecialCharacters( string )</syntax>
     <param name="string">String to be normalized</param>
     <description></description>
     <returns>A normalized string without special characters</returns>
-    <examples>DcCreator.removeSpecialCharacters( "Hvem er du?" )</examples>
+    <examples>Normalize.removeSpecialCharacters( "Hvem er du?" )</examples>
   </doc>;
 
   return that;
