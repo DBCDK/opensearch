44121744c9eef7924b6bd2a83a8591c769cce7b4
lsk <lsk@51cac136-f83d-432d-bb6f-125e83a60a2d>
2012-05-29 11:56:55 +0000

    testing Normalize function in Relations.use.js
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@40895 51cac136-f83d-432d-bb6f-125e83a60a2d

--- scripts/Relations.use.js
+++ scripts/Relations.use.js
@@ -251,9 +251,10 @@ var Relations = function() {
       //Litteratursiden		
 		} else if ( String(analysisXML.dkabm::record.ac::source).match(/Litteratursiden/) && String(analysisXML.dkabm::record.dc::title).match(/Analyse af/) ) {
 			var analysedCreator = analysisXML.dkabm::record.dc::subject[0];
+			analysedCreator = Normalize.removeSpecialCharacters(analysedCreator);
 			var analysedTitle = analysisXML.dkabm::record.dc::subject[1];
 // normalizing seems to cause errors when tested at kuju
-//			analysedTitle = Normalize.removeSpecialCharacters(analysedTitle);
+			analysedTitle = Normalize.removeSpecialCharacters(analysedTitle);
 			Log.debug( "LSK - analysedCreator: " + analysedCreator );
 			Log.debug( "LSK - analysedTitle: " + analysedTitle );
 			var query = "creator = " + analysedCreator + " AND title = " + analysedTitle;
