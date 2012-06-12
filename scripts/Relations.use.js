4e83250a83e9e0bb685152cba1abc65e102aaade
kwc <kwc@51cac136-f83d-432d-bb6f-125e83a60a2d>
2012-06-12 07:57:31 +0000

    changed hasOnlineAccess for oxford to handle more than 1 dc.identifier with dcterms:URI
    git-svn-id: svn://oss.beweb.dbc.dk/sync_search/opensearch/trunk@41231 51cac136-f83d-432d-bb6f-125e83a60a2d

--- scripts/Relations.use.js
+++ scripts/Relations.use.js
@@ -981,7 +981,9 @@ var Relations = function() {
 				DbcAddiRelations.hasOnlineAccess ( pid, "[URL]" + String(manifestationXML.dkabm::record.dc::identifier));
 		} else if (String(manifestationXML.dkabm::record.ac::source).match(/Oxford Reference Online: Premium Collection|Oxford Reference Online: Literature Collection|Oxford Reference Online: Western Collection/)) {
 				for each (child in manifestationXML.dkabm::record.dc::identifier) {
-				DbcAddiRelations.hasOnlineAccess ( pid, "[URL]" + child);
+				   if (String(child.@xsi::type).match("dcterms:URI")) {	
+							DbcAddiRelations.hasOnlineAccess ( pid, "[URL]" + child);
+					}
 				}
 		} else if (String(manifestationXML.dkabm::record.ac::identifier).match(/t[0-9]+\|150031/)) {
 				for each (child in manifestationXML.dkabm::record.dc::identifier) {
